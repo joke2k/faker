943db5b575d71e6793b9505706be1a067abe4867
joke2k <joke2k@gmail.com>
2013-10-18 03:26:36 +0200

    print for python 3

--- faker/__main__.py
+++ faker/__main__.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from __future__ import print_function
 import sys
 
 
@@ -6,18 +7,18 @@ def print_provider(doc, provider, formatters, excludes=None):
     if excludes is None:
         excludes = []
 
-    print
-    print "### faker.providers.{0}".format(doc.get_provider_name(provider))
-    print
+    print()
+    print("### faker.providers.{0}".format(doc.get_provider_name(provider)))
+    print()
 
     for signature, example in formatters.items():
         if signature in excludes:
             continue
-        print "{fake:<{margin}}# {example}".format(
+        print("{fake:<{margin}}# {example}".format(
             fake=signature,
             example=example,
             margin=max(30, doc.max_name_len+1)
-        )
+        ))
 
 
 def main(provider_or_field=None, *args):
@@ -32,12 +33,13 @@ def main(provider_or_field=None, *args):
     if provider_or_field:
         try:
 
-            print fake.format(provider_or_field, *args)
+            print(fake.format(provider_or_field, *args))
             return
         except AttributeError:
             providers = [p for p in fake.providers if doc.get_provider_name(p) == provider_or_field]
             if not providers:
-                return 'No faker found for "{0}"'.format(provider_or_field)
+                print('No faker found for "{0}"'.format(provider_or_field))
+                return
 
             doc.already_generated = base_provider_formatters
             print_provider(doc, providers[0], doc.get_provider_formatters(providers[0]))
@@ -51,8 +53,8 @@ def main(provider_or_field=None, *args):
         for lang in AVAILABLE_LOCALES:
             if lang == DEFAULT_LOCALE:
                 continue
-            print
-            print '############### LANGUAGE {0}'.format(lang)
+            print()
+            print('############### LANGUAGE {0}'.format(lang))
             fake = Faker(locale=lang)
             d = documentor.Documentor(fake)
 
@@ -63,7 +65,7 @@ def main(provider_or_field=None, *args):
 
 if __name__ == "__main__":
     if sys.stdout.encoding is None:
-        print >> sys.stderr, \
-            "please set python env PYTHONIOENCODING=UTF-8, example: export PYTHONIOENCODING=UTF-8, when write to stdout"
+        print("please set python env PYTHONIOENCODING=UTF-8, example: "
+              "export PYTHONIOENCODING=UTF-8, when write to stdout", file=sys.stderr)
         exit(1)
     main(*sys.argv[1:])
