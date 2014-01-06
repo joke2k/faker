a371a58cedd7cc3f12fa20a8e4a2c43f769ca323
Chris Strutton <chris@rodandfly.ca>
2014-01-06 07:13:31 +0000

    Added en_CA local based on Fzaninotto code base

--- faker/__init__.py
+++ faker/__init__.py
@@ -1,4 +1,4 @@
-AVAILABLE_LOCALES = ['en_US', 'it_IT', 'fr_FR', 'pt_BR']
+AVAILABLE_LOCALES = ['en_US', 'it_IT', 'fr_FR', 'pt_BR', 'en_CA']
 
 DEFAULT_LOCALE = AVAILABLE_LOCALES[0]
 
---
+++ faker/providers/en_CA/phone_number.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+from ..phone_number import Provider as PhoneNumberProvider
+
+
+class Provider(PhoneNumberProvider):
+    formats = (
+        '%##-###-####',
+        '%##.###.####',
+        '%## ### ####',
+        '(%##) ###-####',
+        '1-%##-###-####',
+        '1 (%##) ###-####',
+        '+1 (%##) ###-####',
+        '%##-###-#### x###',
+        '(%##) ###-#### x###',
+    )
