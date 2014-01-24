c17a29acf41f98da44868cfdd366466b9a4bff69
GustavAdolf <simone.robutti@gmail.com>
2014-01-24 16:40:37 +0100

    First implementation of profile() and simple_profile()

--- faker/providers/profile.py
+++ faker/providers/profile.py
@@ -1,2 +1,33 @@
-class Profile(object):
-	pass
+from . import BaseProvider
+from .. import Generator
+import itertools
+class Provider(BaseProvider):
+
+
+	def simple_profile(self):
+
+		return {"username":self.generator.user_name(),
+			"name":self.generator.name(),
+			"sex": self.random_element(["M","F"]),
+			"address":self.generator.address(),
+			"mail":self.generator.free_email(),
+
+			#"password":self.generator.password()
+			"birthdate":self.generator.date(),
+
+		}
+
+	def profile(self):
+		d={#"job":self.generator.job(),
+		"ssn":self.numerify("###-##-####"),
+		"residence":self.generator.address(),
+		"current_location":(self.generator.latitude(),self.generator.longitude()),
+		"blood_group":"".join(self.random_element(list(itertools.product(["A","B","AB","0"],["+","-"]))))
+		}
+		for i in range(1,self.random_int(1,5)):
+
+			d["website-"+str(i)]=self.generator.url()
+
+		return dict(d,**self.generator.simple_profile())
+
+
