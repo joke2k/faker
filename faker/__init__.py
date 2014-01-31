27d1b2c99fde99c5b1590dd5dfe4cec2d4b9c615
Marc Tudurí marctc@gmail.com <marctc@gmail.com>
2014-01-31 01:49:19 +0100

    Added es_ES as available locale setting

--- faker/__init__.py
+++ faker/__init__.py
@@ -1,4 +1,4 @@
-AVAILABLE_LOCALES = ['en_US', 'it_IT', 'fr_FR', 'pt_BR', 'en_CA']
+AVAILABLE_LOCALES = ['en_US', 'it_IT', 'fr_FR', 'pt_BR', 'en_CA', 'es_ES']
 
 DEFAULT_LOCALE = AVAILABLE_LOCALES[0]
 
