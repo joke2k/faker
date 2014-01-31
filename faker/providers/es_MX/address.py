387474dab35a6babf3df5c3bfb50922ddac0c618
Espartaco Palma <esparta@gmail.com>
2014-01-30 19:10:34 -0600

    [es_MX] Initial es_MX providers: address & person

--- faker/providers/es_MX/address.py
+++ faker/providers/es_MX/address.py
@@ -1,60 +1,60 @@
+# -*- encoding: utf-8 -*-
 from __future__ import unicode_literals
 from ..address import Provider as AddressProvider
-
+#import locale
 
 class Provider(AddressProvider):
-    city_prefixes = ('North', 'East', 'West', 'South', 'New', 'Lake', 'Port')
-    city_suffixes = (
-        'town', 'ton', 'land', 'ville', 'berg', 'burgh', 'borough', 'bury', 'view', 'port', 'mouth', 'stad', 'furt',
-        'chester', 'mouth', 'fort', 'haven', 'side', 'shire')
+    #current_locale = locale.setlocale(locale.LC_ALL, "es_MX.UTF-8")
+    city_prefixes = ('Sur', 'Norte',)
+    city_adjetives = ('Nueva', 'Vieja',)
+    city_suffixies = ('de la Montaña', 'los bajos', 'los altos', )
+    street_prefixes = (
+        'Ampliación', 'Andador', 'Avenida', 'Boulevard', 'Calle', 'Callejón',
+        'Calzada', 'Cerrada', 'Circuito', 'Circunvalación', 'Continuación',
+        'Corredor', 'Diagonal', 'Eje vial', 'Pasaje', 'Peatonal', 'Periférico',
+        'Privada', 'Prolongación', 'Retorno', 'Viaducto'
+        )
     building_number_formats = ('#####', '####', '###')
-    street_suffixes = (
-        'Alley', 'Avenue', 'Branch', 'Bridge', 'Brook', 'Brooks', 'Burg', 'Burgs', 'Bypass', 'Camp', 'Canyon', 'Cape',
-        'Causeway', 'Center', 'Centers', 'Circle', 'Circles', 'Cliff', 'Cliffs', 'Club', 'Common', 'Corner', 'Corners',
-        'Course', 'Court', 'Courts', 'Cove', 'Coves', 'Creek', 'Crescent', 'Crest', 'Crossing', 'Crossroad', 'Curve',
-        'Dale', 'Dam', 'Divide', 'Drive', 'Drive', 'Drives', 'Estate', 'Estates', 'Expressway', 'Extension',
-        'Extensions',
-        'Fall', 'Falls', 'Ferry', 'Field', 'Fields', 'Flat', 'Flats', 'Ford', 'Fords', 'Forest', 'Forge', 'Forges',
-        'Fork',
-        'Forks', 'Fort', 'Freeway', 'Garden', 'Gardens', 'Gateway', 'Glen', 'Glens', 'Green', 'Greens', 'Grove',
-        'Groves',
-        'Harbor', 'Harbors', 'Haven', 'Heights', 'Highway', 'Hill', 'Hills', 'Hollow', 'Inlet', 'Inlet', 'Island',
-        'Island',
-        'Islands', 'Islands', 'Isle', 'Isle', 'Junction', 'Junctions', 'Key', 'Keys', 'Knoll', 'Knolls', 'Lake',
-        'Lakes',
-        'Land', 'Landing', 'Lane', 'Light', 'Lights', 'Loaf', 'Lock', 'Locks', 'Locks', 'Lodge', 'Lodge', 'Loop',
-        'Mall',
-        'Manor', 'Manors', 'Meadow', 'Meadows', 'Mews', 'Mill', 'Mills', 'Mission', 'Mission', 'Motorway', 'Mount',
-        'Mountain', 'Mountain', 'Mountains', 'Mountains', 'Neck', 'Orchard', 'Oval', 'Overpass', 'Park', 'Parks',
-        'Parkway',
-        'Parkways', 'Pass', 'Passage', 'Path', 'Pike', 'Pine', 'Pines', 'Place', 'Plain', 'Plains', 'Plains', 'Plaza',
-        'Plaza', 'Point', 'Points', 'Port', 'Port', 'Ports', 'Ports', 'Prairie', 'Prairie', 'Radial', 'Ramp', 'Ranch',
-        'Rapid', 'Rapids', 'Rest', 'Ridge', 'Ridges', 'River', 'Road', 'Road', 'Roads', 'Roads', 'Route', 'Row', 'Rue',
-        'Run', 'Shoal', 'Shoals', 'Shore', 'Shores', 'Skyway', 'Spring', 'Springs', 'Springs', 'Spur', 'Spurs',
-        'Square',
-        'Square', 'Squares', 'Squares', 'Station', 'Station', 'Stravenue', 'Stravenue', 'Stream', 'Stream', 'Street',
-        'Street', 'Streets', 'Summit', 'Summit', 'Terrace', 'Throughway', 'Trace', 'Track', 'Trafficway', 'Trail',
-        'Trail',
-        'Tunnel', 'Tunnel', 'Turnpike', 'Turnpike', 'Underpass', 'Union', 'Unions', 'Valley', 'Valleys', 'Via',
-        'Viaduct',
-        'View', 'Views', 'Village', 'Village', 'Villages', 'Ville', 'Vista', 'Vista', 'Walk', 'Walks', 'Wall', 'Way',
-        'Ways', 'Well', 'Wells')
     postcode_formats = ('#####', '#####-####')
     ## States and abbrs from Mexico from INEGI
     ## http://www.inegi.org.mx/geo/contenidos/geoestadistica/CatalogoClaves.aspx
     states = (
-        'Aguascalientes', 'Baja California', 'Baja California Sur', 'Campeche',
-        'Coahuila de Zaragoza', 'Colima', 'Chiapas', 'Chihuahua',
-        'Distrito Federal', 'Durango', 'Guanajuato', 'Guerrero', 'Hidalgo',
-        'Jalisco', 'México', 'Michoacán de Ocampo', 'Morelos', 'Nayarit',
-        'Nuevo León', 'Oaxaca', 'Puebla', 'Querétaro', 'Quintana Roo',
-        'San Luis Potosí', 'Sinaloa', 'Sonora', 'Tabasco', 'Tamaulipas',
-        'Tlaxcala', 'Veracruz de Ignacio de la Llave', 'Yucatán', 'Zacatecas')
-    states_abbr = (
-        'AGS', 'BC', 'BCS', 'CAMP', 'COAH', 'COL', 'CHIS', 'CHIH', 'DF',
-        'DGO', 'GTO', 'GRO', 'HGO', 'JAL', 'MEX', 'MICH', 'MOR', 'NAY',
-        'NL', 'OAX', 'PUE', 'QRO', 'Q. ROO', 'SLP', 'SIN', 'SON', 'TAB',
-        'TAMPS', 'TLAX', 'VER', 'YUC', 'ZAC')
+        ('AGS','Aguascalientes'), ('BC','Baja California'),
+        ('BCS','Baja California Sur'), ('CAMP','Campeche'),
+        ('COAH', 'Coahuila de Zaragoza'), ('COL', 'Colima'),
+        ('CHIS', 'Chiapas'), ('CHIH', 'Chihuahua'),
+        ('DF','Distrito Federal'), ('DGO', 'Durango'),
+        ('GTO', 'Guanajuato'), ('GRO', 'Guerrero'), ('HGO', 'Hidalgo'),
+        ('JAL', 'Jalisco'), ('MEX', 'México'),
+        ('MICH', 'Michoacán de Ocampo'), ('MOR', 'Morelos'),
+        ('NAY', 'Nayarit'), ('NL', 'Nuevo León'), ('OAX', 'Oaxaca'),
+        ('PUE','Puebla'), ('QRO', 'Querétaro'),
+        ('Q. ROO', 'Quintana Roo'), ('SLP', 'San Luis Potosí'),
+        ('SIN', 'Sinaloa'), ('SON', 'Sonora'), ('TAB', 'Tabasco'),
+        ('TAMPS','Tamaulipas'), ('TLAX','Tlaxcala'),
+        ('VER', 'Veracruz de Ignacio de la Llave'),
+        ('YUC', 'Yucatán'), ('ZAC','Zacatecas'),)
+
+    zip_codes = {
+        ## The ZipCodes has a begin & final range
+        ## Source: Norma Técnica de Domicilios INEGI
+        'AGS': (20000, 20999), 'BC' : (21000, 22999),
+        'BCS': (23000, 23999), 'CAMP' : (24000, 24999),
+        'COAH': (25000, 27999), 'COL': (28000, 28999),
+        'CHIS': (29000, 30999), 'CHIH': (31000, 33999),
+        'DF': (1000, 19999), 'DGO': (36000, 35999),
+        'GTO': (36000, 38999), 'GRO': (39000, 41999),
+        'HGO': (42000, 43999), 'JAL': (44000, 49999),
+        'MEX': (50000, 57999), 'MICH': (58000, 61999),
+        'MOR': (62000, 62999), 'NAY': (63000, 63999),
+        'NL': (64000, 67999), 'OAX': (68000, 71999),
+        'PUE': (72000, 75999), 'QRO': (76000, 76999),
+        'Q. ROO': (77000, 75999), 'SLP': (78000, 79999),
+        'SIN': (80000, 82999), 'SON': (83000, 85999),
+        'TAB': (86000, 86999), 'TAMPS': (87000, 89999),
+        'TLAX': (90000, 90999), 'VER': (91000, 97999),
+        'YUC': (97000, 97999), 'ZAC': (98000, 99999)
+    }
     ## List of Countries https://www.un.org/es/members/
     countries = (
         'Afganistán', 'Albania', 'Alemania', 'Andorra', 'Angola',
@@ -107,36 +107,62 @@ class Provider(AddressProvider):
     )
 
     city_formats = (
-        '{{city_prefix}} {{first_name}}{{city_suffix}}',
-        '{{city_prefix}} {{first_name}}',
-        '{{first_name}}{{city_suffix}}',
-        '{{last_name}}{{city_suffix}}',
+        '{{city_adjetive}} {{country}}',
+        'San {{first_name}} {{city_suffix}}',
     )
     street_name_formats = (
-        '{{first_name}} {{street_suffix}}',
-        '{{last_name}} {{street_suffix}}'
+        '{{street_prefix}} {{last_name}}',
+        '{{street_prefix}} {{country}}',
+        '{{street_prefix}} {{state}}',
+        '{{street_prefix}} {{city_prefix}} {{last_name}}'
     )
     street_address_formats = (
-        '{{building_number}} {{street_name}}',
-        '{{building_number}} {{street_name}} {{secondary_address}}',
+        #'{{building_number}} {{street_name}}',
+        '{{street_name}} {{secondary_address}}',
+        #'{{building_number}} {{street_name}} {{secondary_address}}',
     )
     address_formats = (
         "{{street_address}}\n{{city}}, {{state_abbr}} {{postcode}}",
     )
-    secondary_address_formats = ('Apt. ###', 'Suite ###')
+    secondary_address_formats = ('### ###', '### Interior ###',
+        '### Edif. ### , Depto. ###')
 
     @classmethod
     def city_prefix(cls):
         return cls.random_element(cls.city_prefixes)
 
     @classmethod
+    def city_suffix(cls):
+        return cls.random_element(cls.city_suffixies)
+
+    @classmethod
+    def city_adjetive(cls):
+        return cls.random_element(cls.city_adjetives)
+
+    @classmethod
+    def street_prefix(cls):
+        """
+        :example 'Avenida'
+        """
+        return cls.random_element(cls.street_prefixes)
+    @classmethod
     def secondary_address(cls):
+        """
+        :example '020 Interior 999'
+        """
         return cls.numerify(cls.random_element(cls.secondary_address_formats))
 
     @classmethod
     def state(cls):
-        return cls.random_element(cls.states)
+        """
+        example: u'Guerrero'
+        """
+        return cls.random_element(cls.states)[1]
 
     @classmethod
     def state_abbr(cls):
-        return cls.random_element(cls.states_abbr)
+        """
+        example: u'GRO'
+        """
+        return cls.random_element(cls.states)[0]
+
