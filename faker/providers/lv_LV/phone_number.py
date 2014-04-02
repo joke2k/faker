d37024384627cbc7c90dcf5f996f989a5b451367
Edgar Gavrik <edgar.gavrik@gmail.com>
2014-04-02 22:15:44 +0300

    added lv_LV provider

---
+++ faker/providers/lv_LV/phone_number.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+from ..phone_number import Provider as PhoneNumberProvider
+
+
+class Provider(PhoneNumberProvider):
+    formats = (
+        '+371 ## ######',
+        '+371 ########',
+        '+371########',
+        '(+371) ########',
+        '0########',
+        '0# ### ## ##'
+    )
