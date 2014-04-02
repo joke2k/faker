76519dc9eefb1766f06762273ee56306756cfbe8
Edgar Gavrik <edgar.gavrik@gmail.com>
2014-04-02 22:55:25 +0300

    added lt_LT provider

---
+++ faker/providers/lt_LT/phone_number.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+from ..phone_number import Provider as PhoneNumberProvider
+
+
+class Provider(PhoneNumberProvider):
+    formats = (
+        '+370 ## ######',
+        '+370 ########',
+        '+370########',
+        '(+370) ########',
+        '0########',
+        '0# ### ## ##'
+    )
