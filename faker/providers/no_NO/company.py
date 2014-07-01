14aab3fd7dd159715ebb9ed94e6a35b34cdf5cea
kaleissin <kaleissin@gmail.com>
2014-07-01 22:10:58 +0200

    Fixed imports, inheritance, frequencies

--- faker/providers/no_NO/company.py
+++ faker/providers/no_NO/company.py
@@ -1,25 +1,17 @@
 from __future__ import unicode_literals
-from . import BaseProvider
+from ..company import Provider as CompanyProvider
 
 
-class Provider(BaseProvider):
-    formats = ['{{last_name}} {{company_suffix}}', 
-               '{{last_name}}, {{last_name}} og {{last_name}}',
-               '{{last_name}}-{{last_name}}'
-              ]
+class Provider(CompanyProvider):
+    formats = [
+        '{{last_name}} {{company_suffix}}',
+        '{{last_name}} {{company_suffix}}',
+        '{{last_name}} {{company_suffix}}',
+        '{{last_name}}-{{last_name}} {{company_suffix}}',
+        '{{last_name}}, {{last_name}} og {{last_name}}',
+        '{{last_name}}-{{last_name}}'
+    ]
 
-    company_suffixes = ['Gruppen', 'AS', 'ASA', 'BA', 'RFH', 'og Sønner']
-
-    def company(self):
-        """
-        :example 'Acme Ltd'
-        """
-        pattern = self.random_element(self.formats)
-        return self.generator.parse(pattern)
-
-    @classmethod
-    def company_suffix(cls):
-        """
-        :example 'Ltd'
-        """
-        return cls.random_element(cls.company_suffixes)
+    company_suffixes = [
+        'Gruppen', 'AS', 'ASA', 'BA', 'RFH', 'og Sønner', '& co.'
+    ]
