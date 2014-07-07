c9a51fa85f97dc7a55e3a88342db3abe5c86ee44
Flavio Curella <flavio.curella@gmail.com>
2014-07-07 11:32:54 -0500

    better memory management

--- faker/utils/distribution.py
+++ faker/utils/distribution.py
@@ -17,9 +17,9 @@ def choice_distribution(a, p):
     assert len(a) == len(p)
 
     cdf = list(cumsum(p))
-    cdf2 = cdf[:]
-    cdf = [i / cdf[-1] for i in cdf2]
+    normal = cdf[-1]
+    cdf2 = [i / normal for i in cdf]
     uniform_sample = random_sample()
-    idx = bisect.bisect_right(cdf, uniform_sample)
+    idx = bisect.bisect_right(cdf2, uniform_sample)
 
     return a[idx]
