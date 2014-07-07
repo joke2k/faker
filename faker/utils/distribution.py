f7d6ac9423b1ef4574503e0dfc5bb56c63f4c587
Flavio Curella <flavio.curella@gmail.com>
2014-07-07 11:22:37 -0500

    random seed is set globally.

--- faker/utils/distribution.py
+++ faker/utils/distribution.py
@@ -2,9 +2,7 @@ import bisect
 import random
 
 
-def random_sample(seed=None):
-    if seed is not None:
-        random.seed(seed)
+def random_sample():
     return random.uniform(0.0, 1.0)
 
 
@@ -19,13 +17,13 @@ def searchsorted_right(a, v):
     return bisect.bisect_right(a, v)
 
 
-def choice_distribution(a, p, seed=None):
+def choice_distribution(a, p):
     assert len(a) == len(p)
 
     cdf = list(cumsum(p))
     cdf2 = cdf[:]
     cdf = [i / cdf[-1] for i in cdf2]
-    uniform_sample = random_sample(seed)
+    uniform_sample = random_sample()
     idx = searchsorted_right(cdf, uniform_sample)
 
     return a[idx]
