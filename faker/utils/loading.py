00bc0bbbcd790c5a8afbe0568577ffa8a9dd1bab
Flavio Curella <flavio.curella@gmail.com>
2015-01-31 16:14:10 -0600

    exclude directories starting with `_` when listing a module

--- faker/utils/loading.py
+++ faker/utils/loading.py
@@ -3,4 +3,4 @@ import os
 
 def list_module(module):
     path = os.path.dirname(module.__file__)
-    return [i for i in os.listdir(path) if os.path.isdir(os.path.join(path, i))]
+    return [i for i in os.listdir(path) if os.path.isdir(os.path.join(path, i)) and not i.startswith('_')]
