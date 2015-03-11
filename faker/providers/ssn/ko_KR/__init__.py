e43d90455055c4477fa14b80bf5e863c90521a92
Lee Yeonjae <pusungwi@gmail.com>
2015-03-11 11:54:25 +0900

    Update ko_KR locale

---
+++ faker/providers/ssn/ko_KR/__init__.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+from __future__ import unicode_literals
+from .. import Provider as SsnProvider
+
+
+class Provider(SsnProvider):
+    ssn_formats = ("##0#0#-1######", "##0#1#-1######", "##0#2#-1######",
+                   "##0#0#-2######", "##0#1#-2######", "##0#2#-2######",)
+
+    @classmethod
+    def ssn(cls):
+        return cls.bothify(cls.random_element(cls.ssn_formats))
