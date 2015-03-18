joke2k/faker

commit 4bb388fbc901b0eccd7555c403d8e4bba26ebcb7

localized person provider

faker/providers/person/__init__.py
@@ -1,0 +1,107 @@
+localized = True
+
+from .. import BaseProvider
+
+
+class Provider(BaseProvider):
+    formats = ['{{first_name}} {{last_name}}', ]
+
+    first_names = ['John', 'Jane']
+
+    last_names = ['Doe', ]
+
+    def name(self):
+        """
+        :example 'John Doe'
+        """
+        pattern = self.random_element(self.formats)
+        return self.generator.parse(pattern)
+
+    @classmethod
+    def first_name(cls):
+        return cls.random_element(cls.first_names)
+
+    @classmethod
+    def last_name(cls):
+        return cls.random_element(cls.last_names)
+
+    def name_male(self):
+        if hasattr(self, 'formats_male'):
+            formats = self.formats_male
+        else:
+            formats = self.formats
+        pattern = self.random_element(formats)
+        return self.generator.parse(pattern)
+
+    def name_female(self):
+        if hasattr(self, 'formats_female'):
+            formats = self.formats_female
+        else:
+            formats = self.formats
+        pattern = self.random_element(formats)
+        return self.generator.parse(pattern)
+
+    @classmethod
+    def first_name_male(cls):
+        if hasattr(cls, 'first_names_male'):
+            return cls.random_element(cls.first_names_male)
+        return cls.first_name()
+
+    @classmethod
+    def first_name_female(cls):
+        if hasattr(cls, 'first_names_female'):
+            return cls.random_element(cls.first_names_female)
+        return cls.first_name()
+
+    @classmethod
+    def last_name_male(cls):
+        if hasattr(cls, 'last_names_male'):
+            return cls.random_element(cls.last_names_male)
+        return cls.last_name()
+
+    @classmethod
+    def last_name_female(cls):
+        if hasattr(cls, 'last_names_female'):
+            return cls.random_element(cls.last_names_female)
+        return cls.last_name()
+
+
+    @classmethod
+    def prefix(cls):
+        if hasattr(cls, 'prefixes'):
+            return cls.random_element(cls.prefixes)
+        if hasattr(cls, 'prefixes_male') and hasattr(cls, 'prefixes_female'):
+            return cls.random_element((cls.prefixes_male, cls.prefixes_female))
+        return ''
+
+    @classmethod
+    def prefix_male(cls):
+        if hasattr(cls, 'prefixes_male'):
+            return cls.random_element(cls.prefixes_male)
+        return cls.prefix()
+
+    @classmethod
+    def prefix_female(cls):
+        if hasattr(cls, 'prefixes_female'):
+            return cls.random_element(cls.prefixes_female)
+        return cls.prefix()
+
+    @classmethod
+    def suffix(cls):
+        if hasattr(cls, 'suffixes'):
+            return cls.random_element(cls.suffixes)
+        if hasattr(cls, 'suffixes_male') and hasattr(cls, 'suffixes_female'):
+            return cls.random_element((cls.suffixes_male, cls.suffixes_female))
+        return ''
+
+    @classmethod
+    def suffix_male(cls):
+        if hasattr(cls, 'suffixes_male'):
+            return cls.random_element(cls.suffixes_male)
+        return cls.suffix()
+
+    @classmethod
+    def suffix_female(cls):
+        if hasattr(cls, 'suffixes_female'):
+            return cls.random_element(cls.suffixes_female)
+        return cls.suffix()

faker/providers/person/cs_CZ/__init__.py
@@ -1,0 +1,77 @@
+# coding=utf-8
+from __future__ import unicode_literals
+from .. import Provider as PersonProvider
+
+
+class Provider(PersonProvider):
+    formats = (
+        '{{first_name_male}} {{last_name_male}}',
+        '{{first_name_male}} {{last_name_male}}',
+        '{{first_name_male}} {{last_name_male}}',
+        '{{first_name_male}} {{last_name_male}}',
+        '{{first_name_male}} {{last_name_male}}',
+        '{{first_name_female}} {{last_name_female}}',
+        '{{first_name_female}} {{last_name_female}}',
+        '{{first_name_female}} {{last_name_female}}',
+        '{{first_name_female}} {{last_name_female}}',
+        '{{first_name_female}} {{last_name_female}}',
+        '{{prefix_male}} {{first_name_male}} {{last_name_male}}',
+        '{{prefix_female}} {{first_name_female}} {{last_name_female}}',
+        '{{first_name_male}} {{last_name_male}} {{suffix}}',
+        '{{first_name_female}} {{last_name_female}} {{suffix}}',
+        '{{prefix_male}} {{first_name_male}} {{last_name_male}} {{suffix}}',
+        '{{prefix_female}} {{first_name_female}} {{last_name_female}} {{suffix}}'
+    )
+
+
+    first_names_male = (
+        'Adam', 'Alexander', 'Alexandr', 'Aleš', 'Alois', 'Antonín', 'Arnošt', 'Bedřich', 'Bohumil', 'Bohumír', 
+        'Bohuslav', 'Břetislav', 'Dalibor', 'Daniel', 'David', 'Denis', 'Dominik', 'Dušan', 'Eduard', 'Emil', 
+        'Erik', 'Filip', 'František', 'Hynek', 'Igor', 'Ivan', 'Ivo', 'Jakub', 'Jan', 'Jaromír', 'Jaroslav', 
+        'Jindřich', 'Jiří', 'Josef', 'Jozef', 'Ján', 'Kamil', 'Karel', 'Kryštof', 'Ladislav', 'Leoš', 'Libor', 
+        'Lubomír', 'Luboš', 'Ludvík', 'Luděk', 'Lukáš', 'Marcel', 'Marek', 'Marian', 'Martin', 'Matyáš', 'Matěj', 
+        'Michael', 'Michal', 'Milan', 'Miloslav', 'Miloš', 'Miroslav', 'Oldřich', 'Ondřej', 'Otakar', 'Patrik', 'Pavel', 
+        'Peter', 'Petr', 'Přemysl', 'Radek', 'Radim', 'Radomír', 'Radovan', 'René', 'Richard', 'Robert', 'Robin', 'Roman', 
+        'Rostislav', 'Rudolf', 'Samuel', 'Stanislav', 'Tadeáš', 'Tomáš', 'Vasyl', 'Viktor', 'Vilém', 'Vladimír', 'Vladislav', 
+        'Vlastimil', 'Vojtěch', 'Vratislav', 'Václav', 'Vít', 'Vítězslav', 'Zbyněk', 'Zdeněk', 'Šimon', 'Štefan', 'Štěpán'
+    )
+
+    first_names_female = (
+        'Adéla', 'Alena', 'Alexandra', 'Alice', 'Alžběta', 'Andrea', 'Aneta', 'Anežka', 'Anna', 'Barbora', 'Blanka', 'Blažena', 
+        'Bohumila', 'Božena', 'Dagmar', 'Dana', 'Daniela', 'Danuše', 'Denisa', 'Dominika', 'Drahomíra', 'Eliška', 'Emilie', 
+        'Eva', 'Františka', 'Gabriela', 'Hana', 'Helena', 'Ilona', 'Irena', 'Iva', 'Ivana', 'Iveta', 'Jana', 'Jarmila', 
+        'Jaroslava', 'Jindřiška', 'Jitka', 'Jiřina', 'Julie', 'Kamila', 'Karolína', 'Kateřina', 'Klára', 'Kristina', 'Kristýna', 
+        'Květa', 'Květoslava', 'Ladislava', 'Lenka', 'Libuše', 'Lucie', 'Ludmila', 'Magdalena', 'Magdaléna', 'Marcela', 
+        'Marie', 'Markéta', 'Marta', 'Martina', 'Michaela', 'Milada', 'Milena', 'Miloslava', 'Miluše', 'Miroslava', 'Monika', 
+        'Mária', 'Naděžda', 'Natálie', 'Nela', 'Nikol', 'Nikola', 'Olga', 'Pavla', 'Pavlína', 'Petra', 'Radka', 'Renata', 
+        'Renáta', 'Romana', 'Růžena', 'Sabina', 'Simona', 'Soňa', 'Stanislava', 'Sára', 'Tereza', 'Vendula', 'Veronika', 
+        'Viktorie', 'Vladimíra', 'Vlasta', 'Věra', 'Zdenka', 'Zdeňka', 'Zuzana', 'Štěpánka', 'Šárka', 'Žaneta'
+    )
+
+    first_names = first_names_male + first_names_female
+
+    last_names_male = (
+        'Bartoš', 'Beneš', 'Blažek', 'Bláha', 'Doležal', 'Dušek', 'Dvořák', 'Fiala', 'Holub', 'Horák', 'Hájek', 'Jelínek', 
+        'Kadlec', 'Kolář', 'Kopecký', 'Kratochvíl', 'Krejčí', 'Král', 'Kučera', 'Kříž', 'Malý', 'Marek', 'Mareš', 'Mašek', 
+        'Moravec', 'Novotný', 'Novák', 'Němec', 'Pokorný', 'Polák', 'Pospíšil', 'Procházka', 'Růžička', 'Sedláček', 'Soukup', 
+        'Svoboda', 'Urban', 'Vaněk', 'Veselý', 'Vlček', 'Zeman', 'Čermák', 'Černý', 'Říha', 'Šimek', 'Štěpánek', 'Šťastný'
+    )
+
+    last_names_female = (
+        'Bartošová', 'Benešová', 'Beranová', 'Blažková', 'Bláhová', 'Doležalová', 'Dušková', 'Dvořáková', 'Fialová', 'Holubová', 
+        'Horáková', 'Hájková', 'Jandová', 'Jelínková', 'Kadlecová', 'Kolářová', 'Kopecká', 'Kratochvílová', 'Krejčová', 
+        'Králová', 'Kučerová', 'Křížová', 'Machová', 'Malá', 'Marešová', 'Marková', 'Mašková', 'Moravcová', 'Novotná', 
+        'Nováková', 'Němcová', 'Pokorná', 'Poláková', 'Pospíšilová', 'Procházková', 'Růžičková', 'Sedláčková', 'Soukupová', 
+        'Svobodová', 'Tichá', 'Urbanová', 'Vacková', 'Vaňková', 'Veselá', 'Vlčková', 'Vávrová', 'Zemanová', 'Čermáková', 
+        'Černá', 'Říhová', 'Šimková', 'Štěpánková', 'Šťastná'
+    )
+
+    last_names = last_names_male + last_names_female
+
+    degrees = ('JUDr.', 'Ing.', 'Bc.', 'Mgr.', 'MUDr.', 'RNDr.' )
+
+    prefixes_male = ('pan', ) + degrees
+
+    prefixes_female = ('paní', 'slečna', ) + degrees
+
+    suffixes = ('CSc.', 'DiS.', 'Ph.D.', 'Th.D.')

faker/providers/person/de_AT/__init__.py
@@ -1,0 +1,61 @@
+# coding=utf-8
+from __future__ import unicode_literals
+from .. import Provider as PersonProvider
+
+
+class Provider(PersonProvider):
+    formats = (
+        '{{first_name}} {{last_name}}',
+        '{{first_name}} {{last_name}}',
+        '{{first_name}} {{last_name}}',
+        '{{first_name}} {{last_name}}',
+        '{{first_name}} {{last_name}}',
+        '{{prefix}} {{first_name}} {{last_name}}',
+        '{{first_name}} {{last_name}}',
+        '{{prefix}} {{first_name}} {{last_name}}'
+    )
+
+    first_names = (
+        'Alexander', 'Alina', 'Andreas',  'Anna', 'Anton',
+        'Benjamin', 'Bernhard',
+        'Christian', 'Christop',
+        'Daniel', 'David', 'Dominik',
+        'Elena', 'Elias', 'Emil', 'Emilia',
+        'Fabian', 'Felix', 'Florian', 'Franz', 'Fransizka',
+        'Gabriel', 'Gernot',
+        'Hanna',
+        'Ingrid', 'Isabel'
+        'Jakob', 'Jana', 'Jasmin', 'Johanna', 'Johannes', 'Jonas', 'Julia', 'Julian',
+        'Katharinna', 'Konrad', 'Konstantin'
+        'Lara', 'Laura', 'Lena', 'Leo', 'Leon', 'Linda', 'Luca', 'Lukas',
+        'Marcel', 'Maria', 'Martin', 'Matthias', 'Max', 'Maximilian', 'Mia', 'Michael', 'Moritz',
+        'Nico', 'Niklas', 'Nina', 'Noah',
+        'Oliver', 'Olivia',
+        'Paul', 'Paula', 'Philipp', 'Pia',
+        'Raphael', 'Robert',
+        'Samuel', 'Sarah', 'Sebastian', 'Simon', 'Sophie',
+        'Theresa', 'Thomas', 'Tim', 'Tobias',
+        'Valentin'
+    )
+
+    last_names = (
+        'Auer', 'Aigner',
+        'Bauer', 'Baumgartner', 'Berger', 'Binder', 'Brunner',
+        'Cap', 'Capek', 'Cech', 'Chum',
+        'Deng', 'Denk', 'Daume', 'Dienstl',
+        'Ebner', 'Eder', 'Egger',
+        'Fasching', 'Felber', 'Ferstel', 'Fichtner', 'Fischer', 'Fuchs',
+        'Gasser', 'Gastegger', 'Geier', 'Geisler', 'Grabner', 'Gruber',
+        'Haas', 'Haiden', 'Hofer', 'Holzer', 'Huber',
+        'Illes', 'Ircher', 'Itzlinger',
+        'Jahn', 'Jobst', 'Jung', 'Jungbauer', 'Just',
+        'Kainz', 'Karl', 'Karner', 'Koller',
+        'Lang', 'Lechner', 'Lehner', 'Leitner',
+        'Maier', 'Mair', 'Maurer', 'Mayer', 'Mayr', 'Moser', 'Müllner',
+        'Pichler', 'Pucher',
+        'Reiter', 'Riegler',
+        'Schmid', 'Schneider', 'Schuster', 'Schwarz', 'Stadler', 'Steiner',
+        'Wallner',  'Weber', 'Weiss', 'Wieser', 'Wimmer', 'Winkler', 'Winter', 'Wolf',
+    )
+
+    prefixes = ('Dr.', 'Mag.', 'Ing.', 'Dipl.-Ing.', 'Prof.', 'Univ.Prof.')

faker/providers/person/dk_DK/__init__.py
@@ -1,0 +1,125 @@
+# coding=utf-8
+from __future__ import unicode_literals
+from .. import Provider as PersonProvider
+
+class Provider(PersonProvider):
+    formats = (
+        '{{first_name_male}} {{last_name}}',
+        '{{first_name_male}} {{last_name}}',
+        '{{first_name_male}} {{last_name}}',
+        '{{first_name_male}} {{last_name}}',
+        '{{first_name_male}} {{last_name}}-{{last_name}}',
+        '{{first_name_female}} {{last_name}}',
+        '{{first_name_female}} {{last_name}}',
+        '{{first_name_female}} {{last_name}}',
+        '{{first_name_female}} {{last_name}}',
+        '{{first_name_female}} {{last_name}}-{{last_name}}',
+        '{{prefix_male}} {{first_name_male}} {{last_name}}',
+        '{{prefix_female}} {{first_name_female}} {{last_name}}',
+        '{{prefix_male}} {{first_name_male}} {{last_name}}',
+        '{{prefix_female}} {{first_name_female}} {{last_name}}'
+    )
+
+    first_names_male = (
+        'Adam', 'Albert', 'Aksel', 'Alex', 'Alexander', 'Alf', 'Allan',
+        'Alvin', 'Anders', 'André', 'Andreas', 'Anton', 'Arne', 'Asger',
+        'ugust', 'Benjamin', 'Benny', 'Bent', 'Bertil', 'Bertram', 'Birger',
+        'Bjarne', 'Bo', 'Bob', 'Bobby', 'Boe', 'Boris', 'Borris',
+        'Brian', 'Bruno', 'Bøje', 'Børge', 'Carl', 'Carlo', 'Carsten',
+        'Casper', 'Christian', 'Christoffer', 'Christopher', 'Claus', 'Clavs', 'Curt',
+        'Dan', 'Daniel', 'Danny', 'David', 'Dennis', 'Ebbe', 'Einar',
+        'Einer', 'Elias', 'Emil ', 'Eric', 'Erik', 'Erling', 'Ernst',
+        'Esben', 'Finn', 'Flemming ', 'Frank', 'Frans', 'Freddy', 'Frede',
+        'Frederik', 'Frode', 'Georg ', 'George', 'Gert', 'Gorm', 'Gunnar',
+        'Gunner', 'Gustav', 'Hans', 'Helge', 'Henrik', 'Henry', 'Herbert',
+        'Herman', 'Hjalte', 'Holger', 'Hugo', 'Ib', 'Ivan', 'Iver',
+        'Jack', 'Jacob', 'Jakob', 'James', 'Jan', 'Jano', 'Jarl',
+        'Jean', 'Jens', 'Jeppe', 'Jesper', 'Jim', 'Jimmy', 'Joachim',
+        'Joakim', 'Johan', 'Johannes', 'John', 'Johnnie', 'Johnny', 'Jon',
+        'Jonas', 'Jonathan', 'Julius', 'Jørgen', 'Karl', 'Karlo', 'Karsten',
+        'Kaspar', 'Kasper', 'Keld', 'Ken', 'Kenn', 'Kenneth', 'Kenny',
+        'Kent', 'Kim', 'Kjeld', 'Klaus', 'Klavs', 'Kristian', 'Kurt',
+        'Kåre', 'Lars', 'Lasse', 'Laurits', 'Laus', 'Laust', 'Leif',
+        'Lennarth', 'Lucas', 'Ludvig', 'Mads', 'Magnus', 'Malthe', 'Marcus',
+        'Marius', 'Mark', 'Martin', 'Mathias', 'Matthias', 'Michael', 'Mik',
+        'Mikael', 'Mike', 'Mikkel', 'Mogens', 'Morten', 'Nick', 'Nicklas',
+        'Nicolai', 'Nicolaj', 'Niels', 'Nikolai', 'Nikolaj', 'Nils', 'Noah',
+        'Ole', 'Olfert', 'Oliver', 'Oscar', 'Oskar', 'Osvald', 'Otto',
+        'Ove', 'Palle', 'Patrick', 'Paw', 'Peder', 'Per', 'Pete',
+        'Peter', 'Paul', 'Philip', 'Poul', 'Preben', 'Ragnar', 'Ragner',
+        'Rasmus', 'René', 'Richard', 'Richardt', 'Robert', 'Robin', 'Rolf',
+        'Ron', 'Ronni', 'Ronnie', 'Ronny', 'Ruben', 'Rune', 'Sam',
+        'Sebastian', 'Silas', 'Simon', 'Simon', 'Sonny', 'Steen', 'Stefan',
+        'Sten', 'Stephan', 'Steve', 'Steven', 'Stig', 'Svenning', 'Søren',
+        'Tage', 'Tejs', 'Thomas', 'Tim', 'Timmy', 'Tobias', 'Tom',
+        'Tommy', 'Tonny', 'Torben', 'Troels', 'Uffe', 'Ulf', 'Ulrik',
+        'Vagn', 'Valdemar', 'Verner', 'Victor', 'Villads', 'Werner', 'William',
+        'Yan', 'Yannick', 'Yngve', 'Zacharias', 'Ziggy', 'Øivind', 'Øjvind',
+        'Ørni', 'Øvli', 'Øystein', 'Øyvind', 'Åbjørn', 'Aage', 'Åge',
+    )
+
+    first_names_female = (
+        'Abelone', 'Agnes', 'Agnete', 'Alberte', 'Alma', 'Amalie', 'Amanda',
+        'Andrea', 'Ane', 'Anette', 'Anna', 'Anne', 'Annemette', 'Annette',
+        'Asta', 'Astrid', 'Benedicte', 'Benedikte', 'Bente', 'Benthe', 'Berit',
+        'Berta', 'Beth', 'Bettina', 'Birgit', 'Birgitte', 'Birte', 'Birthe',
+        'Bitten', 'Bodil', 'Britt', 'Britta', 'Camilla', 'Carina', 'Carla',
+        'Caroline', 'Cathrine', 'Catrine', 'Cecilie', 'Charlotte', 'Christina', 'Christine',
+        'Cirkeline', 'Clara', 'Connie', 'Conny', 'Dagmar', 'Dagny', 'Daniella',
+        'Dina', 'Ditte', 'Doris', 'Dorte', 'Dorthe', 'Edith', 'Elin',
+        'Elisabeth', 'Ella', 'Ellen', 'Elna', 'Else', 'Elsebeth', 'Emilie',
+        'Emily', 'Emma', 'Erna', 'Esmarelda', 'Ester', 'Filippa', 'Frederikke',
+        'Freja', 'Frida', 'Gerda', 'Gertrud', 'Gitte', 'Grete', 'Grethe',
+        'Gundhild', 'Gunhild', 'Gurli', 'Gyda', 'Hannah', 'Hanne', 'Heidi',
+        'Helen', 'Helle', 'Henriette', 'Herdis', 'Iben', 'Ida', 'Inga',
+        'Inge', 'Ingelise', 'Inger', 'Ingrid', 'Irma', 'Isabella', 'Jacobine',
+        'Jacqueline', 'Janne', 'Janni', 'Jannie', 'Jasmin', 'Jean', 'Jenny',
+        'Joan', 'Johanne', 'Jonna', 'Josefine', 'Josephine ', 'Julie', 'Justina',
+        'Jytte', 'Karen', 'Karin', 'Karina', 'Karla', 'Karoline', 'Katcha',
+        'Katja', 'Katrine', 'Kirsten', 'Kirstin', 'Kirstine', 'Klara', 'Kristina',
+        'Kristine', 'Laura', 'Lea', 'Lena', 'Lene', 'Leonora', 'Line',
+        'Liva', 'Lona', 'Lone', 'Lotte', 'Louise', 'Lærke', 'Maiken',
+        'Maja', 'Majken', 'Malene', 'Malou', 'Maren', 'Margit', 'Margrethe',
+        'Maria', 'Marianne', 'Marie', 'Marlene', 'Mathilde', 'Maya', 'Merete',
+        'Merethe', 'Mette ', 'Mia', 'Michala', 'Michelle', 'Mie', 'Mille',
+        'Mimi', 'Minna', 'Nadia', 'Naja', 'Nana', 'Nanna', 'Nanni',
+        'Natasha', 'Natasja', 'Nete', 'Nicoline', 'Nina', 'Nora', 'Oda',
+        'Odeline', 'Odette', 'Ofelia', 'Olga', 'Olivia', 'Patricia', 'Paula',
+        'Paulina', 'Pernille', 'Pia', 'Ragna', 'Ragnhild', 'Randi', 'Rebecca',
+        'Regitse', 'Regitze', 'Rikke', 'Rita', 'Ritt', 'Ronja', 'Rosa',
+        'Ruth', 'Sabine', 'Sandra', 'Sanne', 'Sara', 'Sarah', 'Selma',
+        'Signe', 'Sigrid', 'Silje', 'Sille', 'Simone', 'Sine', 'Sofia',
+        'Sofie', 'Solveig', 'Solvej', 'Sonja', 'Sophie', 'Stina', 'Stine',
+        'Susanne', 'Sussanne', 'Sussie', 'Sys', 'Sørine', 'Søs', 'Tammy',
+        'Tanja', 'Thea', 'Tilde', 'Tina', 'Tine', 'Tove', 'Trine',
+        'Ulla', 'Ulrike', 'Ursula', 'Vera', 'Victoria', 'Viola', 'Vivian',
+        'Weena', 'Winni', 'Winnie', 'Xenia', 'Yasmin', 'Yda', 'Yrsa',
+        'Yvonne', 'Zahra', 'Zara', 'Zehnia', 'Zelma', 'Zenia', 'Åse',
+    )
+
+    first_names = first_names_male + first_names_female
+
+    last_names = (
+        'Jensen', 'Nielsen', 'Hansen', 'Pedersen', 'Andersen', 'Christensen', 'Larsen',
+        'Sørensen', 'Rasmussen', 'Petersen', 'Jørgensen', 'Madsen', 'Kristensen', 'Olsen',
+        'Christiansen', 'Thomsen', 'Poulsen', 'Johansen', 'Knudsen', 'Mortensen', 'Møller',
+        'Jacobsen', 'Jakobsen', 'Olesen', 'Frederiksen', 'Mikkelsen', 'Henriksen', 'Laursen',
+        'Lund', 'Schmidt', 'Eriksen', 'Holm', '	Kristiansen', 'Clausen', 'Simonsen',
+        'Svendsen', 'Andreasen', 'Iversen', 'Jeppesen', 'Mogensen', 'Jespersen', 'Nissen',
+        'Lauridsen', 'Frandsen', 'Østergaard', 'Jepsen', 'Kjær', 'Carlsen', 'Vestergaard',
+        'Jessen', 'Nørgaard', 'Dahl', 'Christoffersen', 'Skov', 'Søndergaard', 'Bertelsen',
+        'Bruun', 'Lassen', 'Bach', 'Gregersen', 'Friis', 'Johnsen', 'Steffensen',
+        'Kjeldsen', 'Bech', 'Krogh', '	Lauritsen', 'Danielsen', 'Mathiesen', 'Andresen',
+        'Brandt', 'Winther', 'Toft', 'Ravn', 'Mathiasen', 'Dam', 'Holst',
+        'Nilsson', 'Lind', 'Berg', 'Schou', 'Overgaard', 'Kristoffersen', 'Schultz',
+        'Klausen', 'Karlsen', 'Paulsen', 'Hermansen', 'Thorsen', 'Koch', 'Thygesen',
+    )
+
+    prefixes_male = (
+        'Hr', 'Dr.', 'Prof.', 'Univ.Prof.'
+    )
+
+    prefixes_female = (
+        'Fru', 'Dr.', 'Prof.', 'Univ.Prof.'
+    )
+

commit 1364c69a2daac24fc8c5902a7aae97118e1b8fc8

Make Person `prefixes`/`suffixes` always return strings.
Fixes regression introduced in 4bb388f that made it return lists/tuples in
the presence of `prefixes_male`/`prefixes_female`.

Should resolve #202.

faker/providers/person/__init__.py
@@ -71,7 +71,8 @@ class Provider(BaseProvider):
         if hasattr(cls, 'prefixes'):
             return cls.random_element(cls.prefixes)
         if hasattr(cls, 'prefixes_male') and hasattr(cls, 'prefixes_female'):
-            return cls.random_element((cls.prefixes_male, cls.prefixes_female))
+            prefixes = cls.random_element((cls.prefixes_male, cls.prefixes_female))
+            return cls.random_element(prefixes)
         return ''
 
     @classmethod
@@ -91,7 +92,8 @@ class Provider(BaseProvider):
         if hasattr(cls, 'suffixes'):
             return cls.random_element(cls.suffixes)
         if hasattr(cls, 'suffixes_male') and hasattr(cls, 'suffixes_female'):
-            return cls.random_element((cls.suffixes_male, cls.suffixes_female))
+            suffixes = cls.random_element((cls.suffixes_male, cls.suffixes_female))
+            return cls.random_element(suffixes)
         return ''
 
     @classmethod

faker/tests/__init__.py
@@ -16,6 +16,11 @@ except ImportError:
 from faker import Generator, Factory
 from faker.utils import text, decorators
 
+try:
+    string_types = (basestring,)
+except NameError:
+    string_types = (str,)
+
 
 TEST_DIR = os.path.dirname(__file__)
 
@@ -245,6 +250,14 @@ class FactoryTestCase(unittest.TestCase):
         # test that certain formatting strings are allowed on post-1900 dates
         result = datetime_safe.date(2008, 2, 29).strftime('%y')
         self.assertEqual(result, r'08')
+
+    def test_prefix_suffix_always_string(self):
+        # Locales known to contain `*_male` and `*_female`.
+        for locale in ("bg_BG", "dk_DK", "en", "ru_RU", "tr_TR"):
+            f = Factory.create(locale=locale)
+            for x in range(20):  # Probabilistic testing.
+                assert isinstance(f.prefix(), string_types)
+                assert isinstance(f.suffix(), string_types)
 
 
 if __name__ == '__main__':