e45a19ac677b5237765bbfa137b36434d4e24055
Tatsuji Tsuchiya <ta2xeo@gmail.com>
2015-03-30 15:29:25 +0900

    Add localized providers ja_JP

---
+++ faker/providers/phone_number/ja_JP/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+from .. import Provider as PhoneNumberProvider
+
+
+class Provider(PhoneNumberProvider):
+    formats = (
+        '070-####-####',
+        '080-####-####',
+        '090-####-####',
+        '##-####-####',
+    )
