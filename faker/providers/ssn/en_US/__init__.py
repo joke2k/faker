7aeb95324189537c7a080b7dea84fb6613c32dd4
Joe Schafer <joe@jschaf.com>
2015-07-09 02:10:13 -0400

    Fix U.S. SSN generation
    Certain numbers are invalid for U.S. SSNs. The area (first 3 digits)
    cannot be 666 or 900-999. The group number (middle digits) cannot be 00.
    The serial (last 4 digits) cannot be 0000.
    
    See http://www.socialsecurity.gov/employer/randomizationfaqs.html#a0=5
    for details.

--- faker/providers/ssn/en_US/__init__.py
+++ faker/providers/ssn/en_US/__init__.py
@@ -4,4 +4,17 @@ from .. import Provider as BaseProvider
 
 
 class Provider(BaseProvider):
-    pass
+
+    @classmethod
+    def ssn(cls):
+        # Certain numbers are invalid for U.S. SSNs. The area (first 3 digits)
+        # cannot be 666 or 900-999. The group number (middle digits) cannot be
+        # 00. The serial (last 4 digits) cannot be 0000
+        area = BaseProvider.random_int(min=1, max=899)
+        if area == 666:
+            area += 1
+        group = BaseProvider.random_int(1, 99)
+        serial = BaseProvider.random_int(1, 9999)
+
+        ssn = "{0:03d}-{1:02d}-{2:04d}".format(area, group, serial)
+        return ssn
