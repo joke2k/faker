ac798aaf827611e7baaedf62a3212354895226cc
danhuss <danhuss@users.noreply.github.com>
2015-12-11 10:22:24 -0500

    Added course titles and re-ordered subjects.

--- faker/providers/course/__init__.py
+++ faker/providers/course/__init__.py
@@ -1,9 +1,30 @@
 from .. import BaseProvider
 
 class Provider(BaseProvider):
-	subjects=["English","Art","Computer Science","Music"]
+	subjects=["Art","Computer Science","English","Music"]
 	
 	grades=["A","A-","B+","B","B-","C+","C","C-","D+","D","D-","F"]
+	
+	titles=["Advanced Loitering",
+			"Class 101",
+			"Dirty Pictures",
+			"Flipping Burgers for Fun and Profit", 
+			"Getting Dressed",
+			"History of Something",
+			"How to Watch Television",
+			"Introduction to Basics",
+			"Joy of Garbage",
+			"Learning!",
+			"Philosophy and Star Trek",
+			"Philosophy of the Simposons",
+			"Rocket Surgery",
+			"The Art of Walking",
+			"The Beatles",
+			"The Science of Superheroes",
+			"The Strategy of Starcraft",
+			"Theoretical Phys Ed",
+			"Tree Climbing",
+			"Underwater Basketweaving"]
 
 	@classmethod
 	def subject(cls):
@@ -12,3 +33,7 @@ class Provider(BaseProvider):
 	@classmethod
 	def grade(cls):
 		return cls.random_element(cls.grades)
+		
+	@classmethod
+	def title(cls):
+		return cls.random_element(cls.titles)
