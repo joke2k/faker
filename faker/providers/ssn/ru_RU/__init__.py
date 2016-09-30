99e30459ec66ce104b9df83120bea3ac11ce6e97
Aleksey Lezhoev <lezhoev@gmail.com>
2016-09-30 16:45:53 +0300

    SSN provider for ru_RU locale

---
+++ faker/providers/ssn/ru_RU/__init__.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+from __future__ import unicode_literals
+from .. import Provider as SsnProvider
+
+class Provider(SsnProvider):
+    ssn_formats = ("############",)
+
+    @classmethod
+    def ssn(cls):
+        return cls.numerify(cls.random_element(cls.ssn_formats))
