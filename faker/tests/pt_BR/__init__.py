f3db26775a2eda50f9ddc2d5d73eec623af51168
Luiz Menezes <luiz.menezesf@gmail.com>
2016-10-27 23:04:28 -0200

    Test pt_BR company id (CNPJ)

--- faker/tests/pt_BR/__init__.py
+++ faker/tests/pt_BR/__init__.py
@@ -6,6 +6,7 @@ import unittest
 import re
 
 from faker import Factory
+from faker.providers.company.pt_BR import Provider as CompanyProvider, company_id_checksum
 from faker.providers.ssn.pt_BR import Provider, checksum
 
 
@@ -18,9 +19,22 @@ class pt_BR_FactoryTestCase(unittest.TestCase):
         self.assertEqual(checksum([8, 8, 2, 8, 2, 1, 6, 5, 2, 2]), 1)
 
     def test_pt_BR_ssn(self):
-        for i in range(100):
+        for _ in range(100):
             self.assertTrue(re.search(r'^\d{11}$', Provider.ssn()))
 
     def test_pt_BR_cpf(self):
-        for i in range(100):
+        for _ in range(100):
             self.assertTrue(re.search(r'\d{3}\.\d{3}\.\d{3}\-\d{2}', Provider.cpf()))
+
+    def test_pt_BR_company_id_checksum(self):
+        self.assertEqual(company_id_checksum([9, 4, 9, 5, 3, 4, 4, 1, 0, 0, 0, 1]), [5, 1])
+        self.assertEqual(company_id_checksum([1, 6, 0, 0, 4, 6, 3, 9, 0, 0, 0, 1]), [8, 5])
+
+    def test_pt_BR_company_id(self):
+        for _ in range(100):
+            self.assertTrue(re.search(r'^\d{14}$', CompanyProvider.company_id()))
+
+    def test_pt_BR_cnpj(self):
+        for _ in range(100):
+            cnpj = CompanyProvider.cnpj()
+            self.assertTrue(re.search(r'\d{2}\.\d{3}\.\d{3}/0001-\d{2}', cnpj))
