d7ed1fc458ce277390ea990a37ad0d344ce44b6e
Ahmet Novalić <ahmetnovalic@gmail.com>
2016-10-31 22:37:01 +0000

    Added sources

--- faker/providers/person/bs_BA/__init__.py
+++ faker/providers/person/bs_BA/__init__.py
@@ -5,6 +5,12 @@ from .. import Provider as PersonProvider
 
 class Provider(PersonProvider):
     formats = ['{{first_name}} {{last_name}}', ]
+    
+     """
+    The names used here are the top 100 names released by the Federal statistics institute
+    in Bosnia and Herzegovina, available at 
+    * http://fzs.ba/index.php/2016/05/29/najcesca-dodjeljivana-imena-novorodene-djece-u-2015-godini/
+    """
 
     first_names_male = [
         "Ahmet", "Amar", "Alen", "Adin", "Vedad", "Ahmed", "Tarik", 
@@ -42,6 +48,9 @@ class Provider(PersonProvider):
     ]
 
     first_names = first_names_female + first_names_male
+    """
+    Last names were taken from Wikipedia
+    """
     last_names = [
         "Abadžić", "Adilović", "Alečković", "Alomerović", "Arnautović", 
         "Babić", "Bajraktarević", "Bajramović", "Balašević", "Baljić", 
