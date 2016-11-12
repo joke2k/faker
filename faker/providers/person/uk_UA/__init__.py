6afa2ec764bfab5c8408965ecedda6e06ab37f90
Illia Volochii <illia.volochii@gmail.com>
2016-11-12 22:19:43 +0200

    Create `first_names` in the Ukrainian person provider

--- faker/providers/person/uk_UA/__init__.py
+++ faker/providers/person/uk_UA/__init__.py
@@ -38,6 +38,8 @@ class Provider(PersonProvider):
         'Христина', 'Ярина', 'Ярослава',
     ]
 
+    first_names = first_names_male + first_names_female
+
     # Ukrainian last names are taken from
     # http://uk.wikipedia.org/wiki/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D1%96%D1%8F:%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D1%81%D1%8C%D0%BA%D1%96_%D0%BF%D1%80%D1%96%D0%B7%D0%B2%D0%B8%D1%89%D0%B0
     last_names = [
