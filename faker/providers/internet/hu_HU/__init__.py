1ddd15e2afbeef612745f47119248120f4b6db54
Juha Remes <jremes@outlook.com>
2017-04-29 17:32:54 +0100

    Improved hu_HU accuracy

---
+++ faker/providers/internet/hu_HU/__init__.py
@@ -0,0 +1,33 @@
+# coding=utf8
+from __future__ import unicode_literals
+from .. import Provider as InternetProvider
+
+class Provider(InternetProvider):
+
+    free_email_domains = (
+        'gmail.com',
+        'hotmail.com',
+        'yahoo.com'
+    )
+
+    tlds = (
+        'hu',
+        'com',
+        'com.hu',
+        'info',
+        'org',
+        'net',
+        'biz'
+    )
+
+    replacements = (
+        ('ö', 'o'),
+        ('ü', 'u'),
+        ('á', 'a'),
+        ('é', 'e'),
+        ('í', 'i'),
+        ('ó', 'i'),
+        ('ő', 'o'),
+        ('ú', 'u'),
+        ('ű', 'u')
+    )
