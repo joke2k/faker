1f2feb10ccb98915e92ab7b66a1f7687a3bd5604
Jarek Zgoda <jarek.zgoda@gmail.com>
2017-05-29 10:20:38 +0200

    Missing test for pl_PL dow and month name

--- tests/providers/date_time.py
+++ tests/providers/date_time.py
@@ -6,6 +6,7 @@ import unittest
 from faker import Factory
 from .. import string_types
 
+
 class TestHuHU(unittest.TestCase):
     """ Tests date_time in hu_HU locale. """
 
@@ -20,3 +21,41 @@ class TestHuHU(unittest.TestCase):
         month = self.factory.month()
         assert isinstance(month, string_types)
 
+
+class TestPlPL(unittest.TestCase):
+
+    DAY_NAMES = (
+        'poniedziałek',
+        'wtorek',
+        'środa',
+        'czwartek',
+        'piątek',
+        'sobota',
+        'niedziela',
+    )
+
+    MONTH_NAMES = (
+        'styczeń',
+        'luty',
+        'marzec',
+        'kwiecień',
+        'maj',
+        'czerwiec',
+        'lipiec',
+        'sierpień',
+        'wrzesień',
+        'październik',
+        'listopad',
+        'grudzień'
+    )
+
+    def setUp(self):
+        self.factory = Factory.create('pl_PL')
+
+    def test_day(self):
+        day = self.factory.day_of_week()
+        assert day in self.DAY_NAMES
+
+    def test_month(self):
+        month = self.factory.month_name()
+        assert month in self.MONTH_NAMES
