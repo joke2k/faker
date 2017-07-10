7516126ed9528236a428d466230727bfc5a952fc
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2017-07-10 16:29:06 -0400

    Add US License Plate formats

---
+++ faker/providers/license_plate/__init__.py
@@ -0,0 +1,14 @@
+localized = True
+
+from .. import BaseProvider
+from string import ascii_uppercase
+import re
+
+class Provider(BaseProvider):
+    formats = ()
+    @classmethod
+    def license_plate(cls):
+        temp = re.sub(r'\?',
+            lambda x: cls.random_element(ascii_uppercase),
+            cls.random_element(cls.formats))
+        return cls.numerify(temp)
