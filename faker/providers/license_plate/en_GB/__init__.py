dc1210852269122b77099137bdc6aea5dac8e36d
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2017-07-10 16:36:30 -0400

    Add GB and CA license plate formats

---
+++ faker/providers/license_plate/en_GB/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+from .. import Provider as LicensePlateProvider
+
+
+class Provider(LicensePlateProvider):
+    # from https://en.wikipedia.org/wiki/Vehicle_registration_plates_of_the_United_Kingdom
+    formats = (
+            '??## ???',
+            '??##???'
+        )
