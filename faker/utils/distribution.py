07823ae7f7368f4bc4a4e4436129319f7215150b
Cat Lee Ball <Lee.Cat.Ball@gmail.com>
2017-07-11 09:19:55 -0700

    Use random.choices when available for better performance

--- faker/utils/distribution.py
+++ faker/utils/distribution.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
 import bisect
+from sys import version_info
 from faker.generator import random
 
 def random_sample():
@@ -17,10 +18,13 @@ def cumsum(it):
 def choice_distribution(a, p):
     assert len(a) == len(p)
 
-    cdf = list(cumsum(p))
-    normal = cdf[-1]
-    cdf2 = [float(i) / float(normal) for i in cdf]
-    uniform_sample = random_sample()
-    idx = bisect.bisect_right(cdf2, uniform_sample)
-
-    return a[idx]
+    if version_info.major >= 3 and version_info.minor >= 6:
+        from random import choices
+        return choices(a, weights=p)[0]
+    else:
+        cdf = list(cumsum(p))
+        normal = cdf[-1]
+        cdf2 = [float(i) / float(normal) for i in cdf]
+        uniform_sample = random_sample()
+        idx = bisect.bisect_right(cdf2, uniform_sample)
+        return a[idx]
