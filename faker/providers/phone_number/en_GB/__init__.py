af983da02dac72e010139b4c0f27d6daa45318be
Jacob Budin <self@jacobbudin.com>
2017-10-15 13:43:43 -0400

    Add en_GB cellphone formats

--- faker/providers/phone_number/en_GB/__init__.py
+++ faker/providers/phone_number/en_GB/__init__.py
@@ -3,6 +3,15 @@ from .. import Provider as PhoneNumberProvider
 
 
 class Provider(PhoneNumberProvider):
+    # Source: https://en.wikipedia.org/wiki/Telephone_numbers_in_the_United_Kingdom
+
+    cellphone_formats = (
+        '+44 7### ######',
+        '+44 7#########',
+        '07### ######',
+        '07#########',
+    )
+
     formats = (
         '+44(0)##########',
         '+44(0)#### ######',
@@ -15,3 +24,7 @@ class Provider(PhoneNumberProvider):
         '(0####) ######',
         '(0####) #####',
     )
+
+    def cellphone_number(self):
+        pattern = self.random_element(self.cellphone_formats)
+        return self.numerify(self.generator.parse(pattern))
