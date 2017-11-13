081dac55e5049760f14f9cf45d320d6916cf877b
Chris von Csefalvay <chris@chrisvoncsefalvay.com>
2017-11-13 20:54:56 +0100

    Fixed mixed up numbers/digits for the license plate generator.

--- faker/providers/automotive/hu_HU/__init__.py
+++ faker/providers/automotive/hu_HU/__init__.py
@@ -7,5 +7,5 @@ from .. import Provider as AutomotiveProvider
 class Provider(AutomotiveProvider):
     # from https://en.wikipedia.org/wiki/Vehicle_registration_plates_of_Hungary
     license_formats = (
-            '###-???',
+            '???-###',
         )
