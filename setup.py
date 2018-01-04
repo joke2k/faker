cebe54a4ca4362d7dd47fbbd51c456f2ea4c4cd0
Jon Dufresne <jon.dufresne@gmail.com>
2018-01-03 21:21:38 -0800

    Doc faker as stable and ready for production use

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     },
     classifiers=[
         # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
