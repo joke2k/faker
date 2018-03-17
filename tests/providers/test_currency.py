f7fcf82434f345356a8c3558fdd4cab2fad099e3
Anthony Monthe <anthony.monthe@gmail.com>
2018-03-17 18:00:53 +0000

    Added Currencies' names

--- tests/providers/test_currency.py
+++ tests/providers/test_currency.py
@@ -8,6 +8,22 @@ class TestCurrencyProvider(unittest.TestCase):
     def setUp(self):
         self.factory = Faker()
 
+    def test_currency(self):
+        for _ in range(99):
+            code = self.factory.currency()
+            self.assertIsInstance(code, tuple)
+            self.assertIn(code, CurrencyProvider.currencies)
+
+    def test_currency_code(self):
+        for _ in range(99):
+            code = self.factory.currency_code()
+            self.assertIsInstance(code, str)
+
+    def test_currency_name(self):
+        for _ in range(99):
+            code = self.factory.currency_name()
+            self.assertIsInstance(code, str)
+
     def test_cryptocurrency_code(self):
         for _ in range(99):
             code = self.factory.cryptocurrency_code()
