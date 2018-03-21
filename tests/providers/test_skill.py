012626eb6faa06d2a06451d33515fc29a26741b5
Lane012 <lane.hensley@hotmail.com>
2018-03-20 20:05:59 -0500

    unit test to make sure Skills are mapped by type correctly

---
+++ tests/providers/test_skill.py
@@ -0,0 +1,15 @@
+import unittest
+from faker.providers.skill.en_US import Provider as EnUsProvider
+from faker import Faker
+
+
+class TestSkill(unittest.TestCase):
+
+    def setUp(self):
+        self.factory = Faker('en_US')
+
+    def test_skill_provider_grabs_soft_skill(self):
+        skill = self.factory.soft_skill()
+
+        assert skill in EnUsProvider.soft_skills
+
