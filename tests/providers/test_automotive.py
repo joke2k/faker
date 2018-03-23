8d28a966b9eb4712ef4964ffa001f6d15027a26b
dunz <dunz@trollbox.info>
2018-03-23 16:29:06 +0100

    add test for automotive de_DE

--- tests/providers/test_automotive.py
+++ tests/providers/test_automotive.py
@@ -28,3 +28,13 @@ class TestHuHU(unittest.TestCase):
         plate = self.factory.license_plate()
 
         assert re.match("[A-Z]{3}-\d{3}", plate)
+
+class TestDeDe(unittest.TestCase):
+
+    def setUp(self):
+        self.factory = Faker('de_DE')
+
+    def test_de_DE_plate_format(self):
+        plate = self.factory.license_plate()
+
+        assert re.match("[A-Z]{1,3}-[A-Z]{1,2}-\d{1,4}", plate)
