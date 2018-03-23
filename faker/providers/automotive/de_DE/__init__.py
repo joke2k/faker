8116770cccd96d6c69f52af5ba5a209a1fc7f05d
dunz <dunz@trollbox.info>
2018-03-23 16:27:50 +0100

    add car license plate format for de_DE

---
+++ faker/providers/automotive/de_DE/__init__.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+
+from __future__ import unicode_literals
+from .. import Provider as AutomotiveProvider
+
+
+class Provider(AutomotiveProvider):
+    license_formats = (
+        '???-??-%@@@',
+        '???-?-%@@@',
+        '??-??-%@@@',
+        '??-?-%@@@',
+        '?-??-%@@@',
+        '?-?-%@@@'
+    )
