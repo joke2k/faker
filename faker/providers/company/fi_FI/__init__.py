341c5108437967eb69e2f0d6e391ce6501dcb219
Tim Sampson <tim@sampson.fi>
2018-03-30 17:52:19 +0300

    cleanup: don't shadow builtins
    Fix except where it would break API

--- faker/providers/company/fi_FI/__init__.py
+++ faker/providers/company/fi_FI/__init__.py
@@ -25,13 +25,13 @@ class Provider(CompanyProvider):
         def calculate_checksum(number):
             """Calculate the checksum using mod 11,2 method"""
             factors = [7, 9, 10, 5, 8, 4, 2]
-            sum = 0
+            sum_ = 0
             for x, y in zip(number, factors):
-                sum = sum + int(x) * y
-            if sum % 11 == 0:
+                sum_ = sum_ + int(x) * y
+            if sum_ % 11 == 0:
                 return '0'
             else:
-                return str(11 - sum % 11)
+                return str(11 - sum_ % 11)
 
         first_digit = str(self.random_digit_not_null())
         body = first_digit + self.bothify(self.random_element(('######',)))
