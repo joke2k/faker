caaa70951aa0b4f5cfa8d53a3fb2256bf08613db
Anthony Monthe <anthony.monthe@gmail.com>
2018-04-06 13:58:50 +0100

    Added Unix device & partition

--- tests/providers/test_file.py
+++ tests/providers/test_file.py
@@ -22,3 +22,27 @@ class TestFile(unittest.TestCase):
             self.assertTrue(re.search(r'\/\w+\/\w+\.pdf', file_path))
             file_path = self.factory.file_path(category='image')
             self.assertTrue(re.search(r'\/\w+\/\w+\.(bmp|gif|jpeg|jpg|png|tiff)', file_path))
+
+    def test_unix_device(self):
+        reg_device = re.compile('^/dev/(vd|sd|xvd)[a-z]$')
+        # Test default
+        for _ in range(100):
+            path = self.factory.unix_device()
+            self.assertTrue(reg_device.match(path))
+        # Test with prefix
+        for _ in range(100):
+            path = self.factory.unix_device('sd')
+            self.assertTrue(reg_device.match(path))
+            self.assertTrue(path.startswith('/dev/sd'))
+
+    def test_unix_partition(self):
+        reg_part = re.compile('^/dev/(vd|sd|xvd)[a-z]\d$')
+        # Test default
+        for _ in range(100):
+            path = self.factory.unix_partition()
+            self.assertTrue(reg_part.match(path))
+        # Test with prefix
+        for _ in range(100):
+            path = self.factory.unix_partition('sd')
+            self.assertTrue(reg_part.match(path))
+            self.assertTrue(path.startswith('/dev/sd'))
