720e3bf941a31eb9ff2c533af46043470d29ed75
jremes <jremes@outlook.com>
2018-10-24 15:13:49 +0100

    Add postcode() method

--- faker/providers/address/de_AT/__init__.py
+++ faker/providers/address/de_AT/__init__.py
@@ -151,6 +151,9 @@ class Provider(AddressProvider):
     def state(self):
         return self.random_element(self.states)
 
+    def postcode(self):
+        return self.bothify(self.random_element(self.postcode_formats))
+
     def latitude(self):
         return self.geo_coordinate(center=47.60707, radius=1)
 
