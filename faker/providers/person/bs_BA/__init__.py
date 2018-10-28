4eda51069adaf108ced0510730a33698e267199b
Ahmet Novalić <ahmetnovalic@gmail.com>
2018-10-28 16:19:37 +0100

    Updated the names for 2017 top names
    Added the sources as well

--- faker/providers/person/bs_BA/__init__.py
+++ faker/providers/person/bs_BA/__init__.py
@@ -6,45 +6,44 @@ from .. import Provider as PersonProvider
 class Provider(PersonProvider):
     """
     The names used here are the top 100 names released by the Federal statistics institute
-    in Bosnia and Herzegovina, available at 
-    * http://fzs.ba/index.php/2016/05/29/najcesca-dodjeljivana-imena-novorodene-djece-u-2015-godini/
+    in Bosnia and Herzegovina, available from demography section at  
+    http://fzs.ba/index.php/statisticke-oblasti/stanovnistvo-i-drustvene-statistike/stanovnistvo-i-registar/
+    male: http://fzs.ba/wp-content/uploads/2018/05/100-najčešćih-muških-imena-dodjeljivanih-djeci-rođenoj-u-2017.pdf
+    female: http://fzs.ba/wp-content/uploads/2018/05/100-najčešćih-ženskih-imena-dodjeljivanih-djeci-rođenoj-u-2017.pdf
     """
 
     formats = ['{{first_name}} {{last_name}}', ]
 
     first_names_male = [
-        "Ahmet", "Amar", "Alen", "Adin", "Vedad", "Ahmed", "Tarik", 
-        "Ajdin", "Harun", "Kenan", "Davud", "Hamza", "Emir", "Imran", 
-        "Daris", "Ajnur", "Muhamed", "Emin", "Armin", "Ivan", "Luka", 
-        "Eman", "Kerim", "Faris", "Benjamin", "Ali", "Demir", "Marko", 
-        "Eldar", "Omar", "Petar", "David", "Edin", "Emrah", "Bilal", 
-        "Adnan", "Ismail", "Nedim", "Anes", "Ante", "Arman", "Danin", 
-        "Adi", "Dženan", "Haris", "Mirza", "Ibrahim", "Aldin", "Filip", 
-        "Bakir", "Amer", "Ensar", "Josip", "Afan", "Džan", "Rijad", 
-        "Eldin", "Abdulah", "Anel", "Mateo", "Mak", "Alem", "Ivano", 
-        "Ammar", "Danis", "Omer", "Amel", "Maid", "Jakov", "Matej", 
-        "Karlo", "Mahir", "Dino", "Emil", "Leon", "Nikola", "Mustafa", 
-        "Adem", "Elmin", "Alen", "Arslan", "Jasmin", "Mehmed", "Almin", 
-        "Ermin", "Mihael", "Din", "Ismar", "Amin", "Adian", "Edvin", 
-        "Faruk", "Jusuf", "Ishak", "Adis", "Aid", "Azur", "Fatih", 
-        "Anis", "Antonio", "Ilhan"
+        "Abdulah", "Adem", "Adi", "Adian", "Adijan", "Adin", "Adnan", "Afan",
+        "Ahmed", "Ajdin", "Ajlan", "Ajnur", "Aldin", "Alem", "Ali", "Almin",
+        "Amar", "Amel", "Amer", "Ammar", "Andrej", "Anes", "Ante", "Anur",
+        "Arian", "Arman", "Armin", "Arslan", "Azur", "Benjamin", "Bilal",
+        "Damir", "Dani", "Danin", "Danis", "Daris", "David", "Davud", "Demir",
+        "Din", "Dino", "Džan", "Džanan", "Dženan", "Edin", "Eldar", "Eldin",
+        "Elmin", "Eman", "Emil", "Emin", "Emir", "Emrah", "Ensar", "Ermin",
+        "Faris", "Faruk", "Fatih", "Filip", "Hamza", "Haris", "Harun",
+        "Ibrahim", "Ilhan", "Imran", "Isak", "Ishak", "Ismail", "Ismar",
+        "Ivan", "Jakov", "Josip", "Jusuf", "Kan", "Kenan", "Kerim", "Leon",
+        "Luka", "Mahir", "Maid", "Mak", "Malik", "Marko", "Matej", "Mateo",
+        "Mihael", "Mirza", "Muhamed", "Mustafa", "Nedim", "Nikola", "Omar",
+        "Omer", "Petar", "Rejjan", "Rijad", "Tarik", "Vedad", "Zejd"
     ]
 
     first_names_female = [
-        "Aysel", "Nudžejma", "Amina", "Lamija", "Sara", "Asja", "Emina", "Lejla", 
-        "Merjem", "Esma", "Ajla", "Nejla", "Adna", "Hana", "Ema", "Ajša", 
-        "Ajna", "Amna", "Sajra", "Ilma", "Iman", "Lana", "Marija", "Amila", 
-        "Nejra", "Ana", "Petra", "Ena", "Nedžla", "Mia", "Ajlin", "Dalila", 
-        "Iva", "Sumeja", "Alina", "Naida", "Medina", "Džana", "Lucija", 
-        "Elma", "Sarah", "Zara", "Azra", "Farah", "Anida", "Dalia", "Ejna", 
-        "Una", "Amra", "Asija", "Šejla", "Džejla", "Aiša", "Elena", "Uma", 
-        "Ilhana", "Jasmina", "Tajra", "Berina", "Matea", "Melisa", "Marta", 
-        "Selma", "Klara", "Melina", "Nadja", "Emma", "Alejna", "Majra", 
-        "Aida", "Ela", "Irma", "Lea", "Nora", "Ivana", "Minela", "Najla", 
-        "Anđela", "Fatima", "Maja", "Nađa", "Nika", "Almina", "Belma", 
-        "Erna", "Lorena", "Dalal", "Nadija", "Zana", "Dženita", "Lejna", 
-        "Eva", "Magdalena", "Amela", "Kanita", "Lajla", "Lara", "Tea", 
-        "Zejneb", "Hena", 
+        "Adna", "Aida", "Aiša", "Ajla", "Ajlin", "Ajna", "Ajša", "Alejna",
+        "Alina", "Amila", "Amina", "Amna", "Amra", "Ana", "Anđela", "Anesa",
+        "Anida", "Asija", "Asja", "Aylin", "Azra", "Belma", "Berina", "Dalal",
+        "Dalia", "Dalija", "Dalila", "Dunja", "Džana", "Džejla", "Džejna",
+        "Ejna", "Ela", "Elena", "Ella", "Elma", "Ema", "Emina", "Emma", "Ena",
+        "Erna", "Esma", "Eva", "Farah", "Fatima", "Hana", "Hanna", "Ilhana",
+        "Ilma", "Iman", "Iva", "Jasmina", "Klara", "Lajla", "Lamija", "Lana",
+        "Lara", "Laura", "Lea", "Lejla", "Lejna", "Lena", "Lucija", "Majra",
+        "Marija", "Marta", "Matea", "Medina", "Melina", "Merjem", "Merjema",
+        "Mia", "Nađa", "Nadija", "Nadja", "Nahla", "Naida", "Najla", "Nedžla",
+        "Nejla", "Nejra", "Nika", "Nora", "Nur", "Petra", "Rita", "Sajra",
+        "Sara", "Sarah", "Šejla", "Selma", "Sumeja", "Tajra", "Uma", "Una",
+        "Zana", "Zara", "Zejneb", "Zerina"
     ]
 
     first_names = first_names_female + first_names_male
@@ -52,21 +51,21 @@ class Provider(PersonProvider):
     Last names were taken from Wikipedia
     """
     last_names = [
-        "Abadžić", "Adilović", "Alečković", "Alomerović", "Arnautović", 
-        "Babić", "Bajraktarević", "Bajramović", "Balašević", "Baljić", 
-        "Bašić", "Bećirović", "Beganović", "Begović", "Berović", "Bešić", 
-        "Bešlagić", "Bičakčić", "Bolić", "Bosnić", "Brunčević", "Burić", 
-        "Čaušević", "Čejvan", "Čengić", "Čorbadžić", "Ćosić", "Delić", 
-        "Demirović", "Divjak", "Dizdarević", "Dodik", "Dugalić", "Džeko", 
-        "Edhemović", "Ferhatović", "Galijašević", "Gudelj", "Hadžić", 
-        "Hadžihalilović", "Handanović", "Hodžić", "Izetbegović", 
-        "Jašić", "Kapić", "Kolašinac", "Kopanja", "Koroman", "Kovač", 
-        "Kurjak", "Kurtćehajić", "Kusturica", "Mahmuljin", 
-        "Majstorović", "Mandić", "Marjanović", "Matković", 
-        "Mehmedbašić", "Mehmedović", "Mešić", "Muratović", 
-        "Mutapčić", "Muftić", "Nadarević", "Nikić", "Nišić", 
-        "Novalić", "Pačariz", "Predojević", "Preldžić", "Rakić", 
-        "Salatić", "Salihović", "Sarajlić", "Šarić", "Sidran", 
-        "Sijerčić", "Sokolović", "Sušić", "Tahirović", "Terzić", 
+        "Abadžić", "Adilović", "Alečković", "Alomerović", "Arnautović",
+        "Babić", "Bajraktarević", "Bajramović", "Balašević", "Baljić",
+        "Bašić", "Bećirović", "Beganović", "Begović", "Berović", "Bešić",
+        "Bešlagić", "Bičakčić", "Bolić", "Bosnić", "Brunčević", "Burić",
+        "Čaušević", "Čejvan", "Čengić", "Čorbadžić", "Ćosić", "Delić",
+        "Demirović", "Divjak", "Dizdarević", "Dodik", "Dugalić", "Džeko",
+        "Edhemović", "Ferhatović", "Galijašević", "Gudelj", "Hadžić",
+        "Hadžihalilović", "Handanović", "Hodžić", "Izetbegović",
+        "Jašić", "Kapić", "Kolašinac", "Kopanja", "Koroman", "Kovač",
+        "Kurjak", "Kurtćehajić", "Kusturica", "Mahmuljin",
+        "Majstorović", "Mandić", "Marjanović", "Matković",
+        "Mehmedbašić", "Mehmedović", "Mešić", "Muratović",
+        "Mutapčić", "Muftić", "Nadarević", "Nikić", "Nišić",
+        "Novalić", "Pačariz", "Predojević", "Preldžić", "Rakić",
+        "Salatić", "Salihović", "Sarajlić", "Šarić", "Sidran",
+        "Sijerčić", "Sokolović", "Sušić", "Tahirović", "Terzić",
         "Topalović", "Topčagić", "Toskić", "Turković", "Zahirović"
-    ]   
+    ]
