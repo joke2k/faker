fe42c6c041b05738e823837989eca00c852eaf88
vilhelmmelkstam <vilhelm.melkstam@gmail.com>
2018-10-31 17:30:20 +0100

    Fixed formating

--- faker/providers/automotive/sv_SE/__init__.py
+++ faker/providers/automotive/sv_SE/__init__.py
@@ -9,8 +9,8 @@ class Provider(AutomotiveProvider):
     # Source: https://en.wikipedia.org/wiki/Vehicle_registration_plates_of_Sweden
     # New possible format: https://goo.gl/gSjsnV
     license_formats = (
-    	# Classic format 
+        # Classic format
         '??? ###',
         # New possible format
-        '??? ##?'
+        '??? ##?',
     )
