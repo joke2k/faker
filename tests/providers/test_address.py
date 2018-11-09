269695598af92abb8ecdb4be6413d65d761a3ff6
Juha Remes <jremes@outlook.com>
2018-11-09 11:16:55 +0000

    Add IrProvider

--- tests/providers/test_address.py
+++ tests/providers/test_address.py
@@ -11,6 +11,7 @@ from ukpostcodeparser.parser import parse_uk_postcode
 from faker import Faker
 from faker.providers.address.de_AT import Provider as DeAtProvider
 from faker.providers.address.de_DE import Provider as DeProvider
+from faker.providers.address.fa_IR import Provider as IrProvider
 from faker.providers.address.el_GR import Provider as GrProvider
 from faker.providers.address.en_AU import Provider as EnAuProvider
 from faker.providers.address.en_CA import Provider as EnCaProvider
