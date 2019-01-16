3e81b7cb2da4a93f31d9dab20c8b2f3646bffe8a
JRBusiness <43971633+JRBusiness@users.noreply.github.com>
2019-01-16 09:32:07 -0800

    Fixed some names.

--- faker/providers/person/vi_VN/__init__.py
+++ faker/providers/person/vi_VN/__init__.py
@@ -2,39 +2,41 @@
 from __future__ import unicode_literals
 from .. import Provider as PersonProvider
 
-
+#Some of the firstname was base on this list: https://en.m.wikipedia.org/wiki/Vietnamese_name
+#Most of them was from my personal experience.
 class Provider(PersonProvider):
     formats = ['{{last_name}}{{first_name}}']
     first_names = [
-        'An', 'An', 'Au',
-        'Bach', 'Banh', 'Bao', 'Bien', 'Bien', 
+        'An', 'An', 'Au', 'Anh',
+        'Bach', 'Banh', 'Beo', 'Bao', 'Bien', 'Bien', 'Buu',
         'Cam', 'Canh', 'Canh', 'Cao', 'Cat', 'Chan', 'Chau', 'Chiem', 'Chu', 'Chung', 'Chu', 'Co', 'Cu', 'Cung', 
-        'Cung', 'Cung', 'Cuu', 
+        'Cuong', 'Chuc', 'Chien', 'Chanh', 'Cung', 'Cuu', 'Co',
         'Dịch', 'Diep', 'Dinh', 'Doan', 'Du', 'Dung', 'Du', 'Duu', 'Dai', 'Dam', 'Dao', 'Dau', 'Diền', 'Dinh', 'Doan', 
-        'Do', 'Dong', 'Dong', 'Duong', 
+        'Do', 'Dong', 'Dong', 'Duong', 'Dang', 'Dang', 'Duy', 'Dieu', 'Dan',
         'Gia', 'Giai', 'Gian', 'Giang', 'Giap', 
-        'Ha', 'Ha', 'Ha', 'Hac', 'Han', 'Hau', 'Hình', 'Hoa', 'Hoac', 'Hoan', 'Hong', 'Hua', 'Huong', 'Hy', 
-        'Kha', 'Khau', 'Khuat', 'Kieu', 'Kim', 'Ky', 'Ky', 
-        'La', 'Lac', 'Lai', 'Lam', 'Lang', 'Lanh', 'LAm', 'Lan', 'Le', 'Lien', 'Lieu', 'Lieu', 'Long', 'Loi', 'Lục', 
-        'Lu', 'Luong', 'Luu', 
+        'Ha', 'Hac', 'Han', 'Hau', 'Hình', 'Hoa', 'Hoac', 'Hoan', 'Hong', 'Hua', 'Huong', 'Hy', 'Hieu', 'Hien',
+        'Kha', 'Khau', 'Khuat', 'Kieu', 'Kim', 'Ky', 'Ky', 'Khanh', 'Khuong', 
+        'La', 'Lac', 'Lau', 'Lai', 'Lam', 'Lang', 'Lanh', 'Lan', 'Le', 'Lien', 'Lieu', 'Long', 'Loi', 'Luc', 
+        'Lu', 'Luong', 'Luu', 'Loi', 'Liem',
         'Ma', 'Mac', 'Mach', 'Mai', 'Manh', 'Mao', 'Man', 'Mieu', 'Mong', 
-        'NgAn', 'Nghe', 'Nghiem', 'Ngu', 'Nguu', 'Nhac', 'Nhan', 'Nham', 'Nhiep', 'Nhieu', 'Nhung', 'Ninh', 'Nong', 
-        'o', 'on', 'ong', 
-        'Phi', 'Pho', 'Phong', 'Phong', 'Phu', 'Phung', 'Phuong', 
-        'Quach', 'Quan', 'Quan', 'Quang', 'Quang', 'Que', 'Quyen', 
-        'Sai', 'Sam', 'Su', 
-        'Ta', 'Tao', 'Tang', 'TAn', 'Tan', 'Tat', 'Te', 
-        'Thach', 'Thai', 'Thai', 'Thang', 'Thanh', 'Thao', 'ThAn', 'Thi', 'Thich', 'Thien', 'Thieu', 'Thoi', 'Thuy', 
+        'Ngab', 'Nghe', 'Nghiem', 'Ngu', 'Nguu', 'Nhac', 'Nhan', 'Nham', 'Nhiep', 'Nhieu', 'Nhung', 'Ninh', 'Nong', 
+        'o', 'on', 'ong',
+        'Phi', 'Pho', 'Phong', 'Phong', 'Phu', 'Phung', 'Phuong', 'Phoi', 'Phat',
+        'Quach', 'Quan', 'Quan', 'Quang', 'Quang', 'Que', 'Quyen', 'Quy', 'Quynh', 'Qui',
+        'Sai', 'Sam', 'Su', 'Sang',
+        'Ta', 'Tao', 'Tang', 'Tan', 'Tat', 'Te', 'Tong', 'Ton',
+        'Thach', 'Thai', 'Thang', 'Thanh', 'Thao', 'Than', 'Thi', 'Thich', 'Thien', 'Thieu', 'Thoi', 'Thuy', 
         'Thu', 'Thuong', 'Tien', 'Tiet', 'Tieu', 'Tieu', 'To', 'Ton', 'Tong', 'Tong', 'Trac', 'Trach', 'Trai', 'Trang', 
-        'Tram', 'TrAu', 'Trì', 'Trieu', 'Trịnh', 'Truong', 'Tu', 'Tuởng', 'Uc', 'ung', 
-        'Van', 'Van', 'VAn', 'Vi', 'Vinh', 'Vu', 'Vuong', 'Vuu', 
-        'Xa', 'Xam', 'Xe', 
+        'Tram', 'Trau', 'Tri', 'Trieu', 'Trinh', 'Truong', 'Tu', 'Tuong', 
+        'Uc', 'Ung', 
+        'Van', 'Vi', 'Vinh', 'Vu', 'Vuong', 'Vuu', 'Vy', 'Vien',
+        'Xa', 'Xam', 
         'Yen'
     ]
 
     last_names = [
-        'Nguyen', 'Tran', 'Le', 'Pham', 'Huynh', 'Hoang', 'Phan', 
-        'Vu', 'Vo', 'Đang', 'Bui', 'Đo', 'Ho', 'Ngo', 'Duong', 'Ly'
+        'Nguyen', 'Luong', 'Tang', 'Diep', 'Tran', 'Le', 'Pham', 'Huynh', 'Hoang', 'Phan', 
+        'Vu', 'Vo', 'Đang', 'Bui', 'Đo', 'Ho', 'Ngo', 'Duong', 'Ly', 'Doan', 'Dinh', 'Duong'
     ]
 
     mid_formats = (
