ecce56ff49ab126b79adb97e2637a320ad37a454
Tobias Zanke <TZanke@users.noreply.github.com>
2019-01-17 16:33:40 +0100

    Review de locale (#896)
    * move country names to new 'de' base
    
    * use identical super methods
    
    * remove line breaks
    
    * sort by first letter by line
    
    * add informational link to german phone numbers

--- faker/providers/phone_number/de_DE/__init__.py
+++ faker/providers/phone_number/de_DE/__init__.py
@@ -5,6 +5,7 @@ from .. import Provider as PhoneNumberProvider
 
 
 class Provider(PhoneNumberProvider):
+    # https://de.wikipedia.org/wiki/Rufnummer#Schreibweisen
     formats = (
         '+49(0)##########',
         '+49(0)#### ######',
