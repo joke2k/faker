ee0ea73cd56a24f44be6ce81d2e228ee179b5660
Flavio Curella <89607+fcurella@users.noreply.github.com>
2019-03-08 14:05:16 -0600

    Allow overriding of random generator (#920)

--- setup.py
+++ setup.py
@@ -74,6 +74,7 @@ setup(
         "mock",
         "pytest>=3.8.0,<3.9",
         "more-itertools<6.0.0",
+        "random2==1.0.1",
     ],
     extras_require={
         ':python_version=="2.7"': [
