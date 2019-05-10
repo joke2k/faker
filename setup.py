7e66bb2b5c741be3349f6fb9d13c1e7d43904a29
Flavio Curella <flavio.curella@gmail.com>
2019-05-10 14:22:35 -0500

    fix appveyor build

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     tests_require=[
         "email_validator>=1.0.1,<1.1.0",
         "ukpostcodeparser>=1.1.1",
-        "mock",
+        "mock ; python_version < '3.3'",
         "pytest>=3.8.0,<3.9",
         "more-itertools<6.0.0 ; python_version < '3.0'",
         "random2==1.0.1",
--- tests/providers/test_internet.py
+++ tests/providers/test_internet.py
@@ -5,8 +5,10 @@ from __future__ import unicode_literals
 from itertools import cycle
 
 import unittest
-
-import mock
+try:
+    from unittest import mock
+except ImportError:
+    import mock
 import pytest
 import six
 
