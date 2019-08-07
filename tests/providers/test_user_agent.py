9a065317880a7525ebf1f211664b1fff657c33c1
gsilvan <silvan@trollbox.info>
2019-08-07 13:57:10 +0200

    add test for user_agent (android|ios) platform token

---
+++ tests/providers/test_user_agent.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+
+from __future__ import unicode_literals
+
+import unittest
+import re
+
+from faker import Faker
+from faker.providers.user_agent import Provider as UaProvider
+
+
+class TestUserAgent(unittest.TestCase):
+    """ Tests user_agent """
+
+    def setUp(self):
+        self.factory = Faker()
+        self.ua = UaProvider(self.factory)
+
+    def test_android_platform_token(self):
+        for _ in range(0, 1000):
+            assert re.match(r"^(Android) (\d+)(\.\d){0,2}", self.ua.android_platform_token())
+
+    def test_ios_platform_token(self):
+        for _ in range(0, 1000):
+            assert re.match(r"^(iPhone|iPad); CPU (iPhone|iPad) OS (\d+)(_\d){0,2} like Mac OS X",
+                            self.ua.ios_platform_token())
