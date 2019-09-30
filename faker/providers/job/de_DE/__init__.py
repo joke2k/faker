046b3ade19cde4855269f58cb66c843e9a8e40fc
Long Nguyen <long.nguyen2306@protonmail.com>
2019-10-01 00:45:31 +0200

    German jobs

---
+++ faker/providers/job/de_DE/__init__.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+from __future__ import unicode_literals
+from .. import Provider as BaseProvider
+
+
+class Provider(BaseProvider):
+    jobs = [
+        'Altenpfleger',
+        'Asphaltbauer',
+        'Artist',
+        'Augenoptiker',
+        'Ausbaufacharbeiter',
+        'Bäcker',
+        'Bankkaufmann',
+        'Beamter',
+        'Binnenschiffer'
+    ]
