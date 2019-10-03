607ed1cd16e98fbce3239aee2d57fc1d1d00cc10
Long Nguyen <long.nguyen2306@protonmail.com>
2019-10-03 16:47:03 +0200

    German jobs (#1003)

---
+++ faker/providers/job/de_DE/__init__.py
@@ -0,0 +1,45 @@
+# coding=utf-8
+from __future__ import unicode_literals
+from .. import Provider as BaseProvider
+
+
+class Provider(BaseProvider):
+    """
+    Source: http://planet-beruf.de/schuelerinnen/mein-beruf/berufe-von-a-z/
+    """
+    jobs = [
+        'Altenpfleger',
+        'Asphaltbauer',
+        'Artist',
+        'Augenoptiker',
+        'Ausbaufacharbeiter',
+        'Bäcker',
+        'Bankkaufmann',
+        'Beamter',
+        'Binnenschiffer',
+        'Chemikant',
+        'Chirurgiemechaniker',
+        'Designer',
+        'Drogist',
+        'Erzieher',
+        'Elektroniker',
+        'Fachinformatiker',
+        'Fleischer',
+        'Florist',
+        'Forstwirst',
+        'Friseur',
+        'Informatiker',
+        'Programmierer',
+        'Techniker',
+        'Mathematiker',
+        'Koreanistiker',
+        'Mechaniker',
+        'Pfleger',
+        'Polizist',
+        'Pilot',
+        'Arzt',
+        'Krankenschwester',
+        'Medizininformatiker',
+        'Schornsteinfeger',
+        'Winzer',
+    ]
