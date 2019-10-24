5aeaba400b139dbfa64eb8efad0c532194238f41
Austin Pua <austinpua@novostormtech.com>
2019-10-24 14:59:10 +0800

    Add automotive provider for PH locales

---
+++ faker/providers/automotive/fil_PH/__init__.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+from ..en_PH import Provider as EnPhAutomotiveProvider
+
+
+class Provider(EnPhAutomotiveProvider):
+    """No difference from Automotive Provider for en_PH locale"""
+    pass
