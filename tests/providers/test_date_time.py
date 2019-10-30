c3b2598b2b0cbac13c17ebae99fb3123c7e64035
jcopps <jeffry.copps@tivo.com>
2019-10-30 21:26:33 +0530

    Tests for other ta_IN providers

--- tests/providers/test_date_time.py
+++ tests/providers/test_date_time.py
@@ -19,6 +19,7 @@ from faker.providers.date_time.pl_PL import Provider as PlProvider
 from faker.providers.date_time.ar_AA import Provider as ArProvider
 from faker.providers.date_time.ar_EG import Provider as EgProvider
 from faker.providers.date_time.hy_AM import Provider as HyAmProvider
+from faker.providers.date_time.ta_IN import Provider as TaInProvider
 
 
 def is64bit():
@@ -663,3 +664,20 @@ class DatesOfBirth(unittest.TestCase):
 
             assert isinstance(dob, date)
             assert days_since_one_hundred_eleven_years_ago > days_since_dob >= days_since_one_hundred_years_ago
+
+
+class TestTaIN(unittest.TestCase):
+    """ Tests date_time in the ta_IN locale """
+
+    def setUp(self):
+        self.factory = Faker('ta_IN')
+
+    def test_day(self):
+        day = self.factory.day_of_week()
+        assert isinstance(day, six.string_types)
+        assert day in TaInProvider.DAY_NAMES.values()
+
+    def test_month(self):
+        month = self.factory.month_name()
+        assert isinstance(month, six.string_types)
+        assert month in TaInProvider.MONTH_NAMES.values()
