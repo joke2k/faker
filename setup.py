e8f9f4da143ca29317ba4bd70a649162fdd2be1b
fcurella <flavio.curella@gmail.com>
2019-11-05 11:50:02 -0600

    drop python 3.4
    # Conflicts:
    #	setup.py

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     platforms=["any"],
     test_suite='tests',
     zip_safe=zip_safe,
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     setup_requires=["pytest-runner"],
     install_requires=[
         "python-dateutil>=2.4",
