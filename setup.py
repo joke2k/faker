bbb179aafd1ef8ad4a04417228af30fa832f038a
Austin Pua <pua.austin.anderson@gmail.com>
2019-12-06 13:38:38 +0800

    Add Python 3.8 CI tests

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
