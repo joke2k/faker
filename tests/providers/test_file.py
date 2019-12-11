b2fb75d1a562c783b7799e93dfdf54bc05c317ef
Austin Pua <pua.austin.anderson@gmail.com>
2019-12-11 21:05:51 +0800

    Standardize tests for file provider

--- tests/providers/test_file.py
+++ tests/providers/test_file.py
@@ -10,28 +10,29 @@ class TestFile(unittest.TestCase):
     """ Tests file """
 
     def setUp(self):
-        self.factory = Faker()
+        self.fake = Faker()
+        Faker.seed(0)
 
     def test_file_path(self):
         for _ in range(100):
-            file_path = self.factory.file_path()
+            file_path = self.fake.file_path()
             assert re.search(r'\/\w+\/\w+\.\w+', file_path)
-            file_path = self.factory.file_path(depth=3)
+            file_path = self.fake.file_path(depth=3)
             assert re.search(r'\/\w+\/\w+\/\w+\.\w+', file_path)
-            file_path = self.factory.file_path(extension='pdf')
+            file_path = self.fake.file_path(extension='pdf')
             assert re.search(r'\/\w+\/\w+\.pdf', file_path)
-            file_path = self.factory.file_path(category='image')
+            file_path = self.fake.file_path(category='image')
             assert re.search(r'\/\w+\/\w+\.(bmp|gif|jpeg|jpg|png|tiff)', file_path)
 
     def test_unix_device(self):
         reg_device = re.compile(r'^/dev/(vd|sd|xvd)[a-z]$')
         # Test default
         for _ in range(100):
-            path = self.factory.unix_device()
+            path = self.fake.unix_device()
             assert reg_device.match(path)
         # Test with prefix
         for _ in range(100):
-            path = self.factory.unix_device('sd')
+            path = self.fake.unix_device('sd')
             assert reg_device.match(path)
             assert path.startswith('/dev/sd')
 
@@ -39,10 +40,10 @@ class TestFile(unittest.TestCase):
         reg_part = re.compile(r'^/dev/(vd|sd|xvd)[a-z]\d$')
         # Test default
         for _ in range(100):
-            path = self.factory.unix_partition()
+            path = self.fake.unix_partition()
             assert reg_part.match(path)
         # Test with prefix
         for _ in range(100):
-            path = self.factory.unix_partition('sd')
+            path = self.fake.unix_partition('sd')
             assert reg_part.match(path)
             assert path.startswith('/dev/sd')
