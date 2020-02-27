f5076803dbea9266c6ec78d235242b8ddf7a9190
Marco <marco@esplores.com>
2020-02-27 09:48:12 +0100

    Added company_vat faker

--- faker/providers/company/it_IT/__init__.py
+++ faker/providers/company/it_IT/__init__.py
@@ -330,6 +330,24 @@ class Provider(CompanyProvider):
 
     company_suffixes = ('SPA', 'e figli', 'Group', 's.r.l.')
 
+    @staticmethod
+    def _luhn_checksum(number):
+        def digits_of(n):
+            return [int(d) for d in str(n)]
+        digits = digits_of(number)
+        odd_digits = digits[-1::-2]
+        even_digits = digits[-2::-2]
+        checksum = 0
+        checksum += sum(odd_digits)
+        for d in even_digits:
+            checksum += sum(digits_of(d * 2))
+        return checksum % 10
+
+    @staticmethod
+    def _calculate_luhn(partial_number):
+        check_digit = Provider._luhn_checksum(int(partial_number) * 10)
+        return check_digit if check_digit == 0 else 10 - check_digit    
+
     def catch_phrase(self):
         """
         :example 'Robust full-range hub'
@@ -349,3 +367,12 @@ class Provider(CompanyProvider):
             result.append(self.random_element(word_list))
 
         return " ".join(result)
+
+
+    def company_vat(self):
+        """
+        Returns Italian VAT identification number (Partita IVA).
+        """
+        code = "0" + self.bothify('######') + str(self.generator.random.randrange(1, 121)).zfill(3)
+        luhn_checksum = str(Provider._calculate_luhn(code))
+        return 'IT{}{}'.format(code, luhn_checksum)
