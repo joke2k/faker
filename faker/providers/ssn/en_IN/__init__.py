b0a34d61fd50384dc3ff8dd6024336f8e563700a
curiousElf <dinunair+github@gmail.com>
2020-03-20 23:24:08 +0530

    Adds support for Indian Aadhaar numbers (#1138)
    * Adds support for Indian Aadhaar numbers
    
    This change also introduces the en_IN locale.
    
    As we needed to use a Luhn  check - moved an existing implementation
    to utils along with additional tests
    
    * Sort imports and changes for flake8 rules
    
    Co-authored-by: Dinu Nair <dinunair@gmail.com>
    Co-authored-by: Dinu Nair <dinu@esthenos.com>

---
+++ faker/providers/ssn/en_IN/__init__.py
@@ -0,0 +1,28 @@
+from faker.utils import checksums
+
+from .. import Provider as BaseProvider
+
+
+class Provider(BaseProvider):
+    """
+    Faker provider for Indian Identifiers
+    """
+
+    aadhaar_id_formats = (
+        '%##########',
+    )
+
+    def aadhaar_id(self):
+        """
+        Aadhaar is a 12 digit person identifier generated for residents of
+        India.
+        Details: https://en.wikipedia.org/wiki/Aadhaar
+        Official Website: https://uidai.gov.in/my-aadhaar/about-your-aadhaar.html
+        """
+
+        aadhaar_digits = self.numerify(self.random_element(self.aadhaar_id_formats))
+        checksum = checksums.calculate_luhn(aadhaar_digits)
+
+        aadhaar_number = '{}{}'.format(aadhaar_digits, checksum)
+
+        return aadhaar_number
