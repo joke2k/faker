7e724ae73e8d120575b036c849e2378a7ffd1640
Yuichiro Smith <contact@yu-smith.com>
2020-05-04 16:46:16 +0900

    JAN docstrings

--- faker/providers/barcode/ja_JP/__init__.py
+++ faker/providers/barcode/ja_JP/__init__.py
@@ -6,10 +6,32 @@ class Provider(BarcodeProvider):
     local_prefixes = (4, 5), (4, 9)
 
     def jan(self, length=13):
+        """
+        Japanese local EAN barcodes are called JAN-codes https://www.dsri.jp/jan/about_jan.html
+        This method is an alias and just calls
+        :meth:`localized_ean()<faker.providers.barcode.Provider.localized_ean>` under the hood
+
+        :param length:
+        :return:
+        """
         return self.localized_ean(length)
 
     def jan8(self):
+        """
+        Japanese local EAN barcodes are called JAN-codes https://www.dsri.jp/jan/about_jan.html
+        This method is an alias and just calls
+        :meth:`localized_ean8()<faker.providers.barcode.Provider.localized_ean8>` under the hood
+
+        :return:
+        """
         return self.localized_ean8()
 
     def jan13(self):
+        """
+        Japanese local EAN barcodes are called JAN-codes https://www.dsri.jp/jan/about_jan.html
+        This method is an alias and just calls
+        :meth:`localized_ean13()<faker.providers.barcode.Provider.localized_ean13>` under the hood
+
+        :return:
+        """
         return self.localized_ean13()
