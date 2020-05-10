11169df096f12a4bc29e354a20a9ff23ce70da79
shervinmo <abtin@riseup.net>
2020-05-10 22:14:34 +0430

    add bill_id generator method

--- faker/providers/bill/fa_IR/__init__.py
+++ faker/providers/bill/fa_IR/__init__.py
@@ -21,21 +21,21 @@ class Bill:
 
 class Provider(BaseProvider):
 
-    water_company_codes = []
-    electricity_company_codes = []
-    gas_company_codes = []
-    phone_company_codes = []
-    mobile_company_codes = []
-    municipality_company_codes = []
+    water_company_codes = ["028"]
+    electricity_company_codes = ["042"]
+    gas_company_codes = ["062"]
+    phone_company_codes = ["211"]
+    mobile_company_codes = ["301"]
+    municipality_company_codes = ["009"]
 
     # https://www.tavanir.org.ir/dm/dmmoshtarekin/doc/bill850530.pdf
     bill_types = OrderedDict((
-        ('water', Bill('قبض آب', 'آبفا', 1, water_company_codes)),
-        ('electricity', Bill('قبض برق', 'توانیر', 2, electricity_company_codes)),
-        ('gas', Bill('قبض گاز', 'شرکت گاز', 3, gas_company_codes)),
-        ('phone', Bill('قبض تلفن', 'مخابرات', 4, phone_company_codes)),
-        ('mobile', Bill('قبض موبایل', 'همراه اول', 5, mobile_company_codes)),
-        ('municipality', Bill('قبض عوارض شهرداری', 'شهرداری', 6, municipality_company_codes)),
+        ('water', Bill('قبض آب', 'آبفا', "1", water_company_codes)),
+        ('electricity', Bill('قبض برق', 'توانیر', "2", electricity_company_codes)),
+        ('gas', Bill('قبض گاز', 'شرکت گاز', "3", gas_company_codes)),
+        ('phone', Bill('قبض تلفن', 'مخابرات', "4", phone_company_codes)),
+        ('mobile', Bill('قبض موبایل', 'همراه اول', "5", mobile_company_codes)),
+        ('municipality', Bill('قبض عوارض شهرداری', 'شهرداری', "6", municipality_company_codes)),
     ))
 
     def bill_provider(self, bill_type=None):
@@ -50,6 +50,14 @@ class Provider(BaseProvider):
             bill_type = self.random_element(self.bill_types.keys())
         return self._bill_types(bill_type).name
 
+    def bill_id(self, bill_type=None, company_code=None, client_case_code=None, provider_code=None):
+        """ Returns a bill_id instance. """
+        bill_id = self._bill_id(
+            bill_type=bill_type, company_code=company_code,
+            client_case_code=client_case_code, provider_code=provider_code
+        )
+        return bill_id + str(self._make_control_number(bill_id))
+
     def _bill_types(self, bill_type=None):
         """ Returns a random bill instance. """
         if bill_type is None:
@@ -57,3 +65,27 @@ class Provider(BaseProvider):
         elif isinstance(bill_type, Bill):
             return bill_type
         return self.bill_types[bill_type]
+
+    def _bill_id(self, bill_type=None, company_code=None, client_case_code=None, provider_code=None):
+        bill = self._bill_types(bill_type)
+        if not company_code:
+            company_code = self.random_element(bill.company_codes)
+        if not client_case_code:
+            client_case_code = str(self.random_int(100, 10000000))
+        if not provider_code:
+            provider_code = bill.provider_code
+        bill_id = client_case_code + company_code + provider_code
+        return bill_id
+
+    @staticmethod
+    def _make_control_number(bill_id):
+        sum_number = 0
+        ctrl_number = 2
+        for number in bill_id[::-1]:
+            sum_number += int(number) * ctrl_number
+            if ctrl_number == 7:
+                ctrl_number = 2
+                continue
+            ctrl_number += 1
+        sum_number = sum_number % 11
+        return 11 - sum_number if sum_number > 1 else 0
