4ad7820dc6105173f9a2af70c430a3c5600ed08f
shervinmo <abtin@riseup.net>
2020-05-10 19:36:23 +0430

    add methods for bill provider and bill name

--- faker/providers/bill/fa_IR/__init__.py
+++ faker/providers/bill/fa_IR/__init__.py
@@ -11,13 +11,12 @@ class Bill:
             name,
             provider,
             provider_code,
-            company_code,
-            
+            company_codes,
             ):
         self.name = name
         self.provider = provider
         self.provider_code = provider_code
-        self.company_code = company_code
+        self.company_codes = company_codes
 
 
 class Provider(BaseProvider):
@@ -38,3 +37,23 @@ class Provider(BaseProvider):
         ('mobile', Bill('قبض موبایل', 'همراه اول', 5, mobile_company_codes)),
         ('municipality', Bill('قبض عوارض شهرداری', 'شهرداری', 6, municipality_company_codes)),
     ))
+
+    def bill_provider(self, bill_type=None):
+        """ Returns the name of the bill. """
+        if bill_type is None:
+            bill_type = self.random_element(self.bill_types.keys())
+        return self._bill_types(bill_type).provider
+
+    def bill_name(self, bill_type=None):
+        """ Returns the provider's name of the bill. """
+        if bill_type is None:
+            bill_type = self.random_element(self.bill_types.keys())
+        return self._bill_types(bill_type).name
+
+    def _bill_types(self, bill_type=None):
+        """ Returns a random bill instance. """
+        if bill_type is None:
+            bill_type = self.random_element(self.bill_types.keys())
+        elif isinstance(bill_type, Bill):
+            return bill_type
+        return self.bill_types[bill_type]
