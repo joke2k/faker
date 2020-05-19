fca4fe9eb353eb500bc24a9c401bffbf0883565d
malefice <pua.austin.anderson@gmail.com>
2020-05-19 22:26:59 +0800

    Test Suite Housekeeping (Part 3)  (#1167)
    * Add test fixtures exclusive for tests.providers
    
    * Pytestify tests.sphinx
    
    * Pytestify and improve user agent provider tests
    
    * Pytestify barcode provider tests
    
    * Fix flake8 bare except issue
    
    * Pytestify, reorganize, and improve Generator class tests
    
    * Pytestify, reorganize, and improve Faker class tests
    
    * Fix test failure
    
    * Pytestify currency provider tests
    
    * Pytestify isbn provider tests
    
    * Pytestify, reorganize, and improve internet provider tests
    
    * Pytestify, reorganize, and improve base provider tests
    
    * Fix CI errors

---
+++ tests/providers/conftest.py
@@ -0,0 +1,41 @@
+import locale as pylocale
+import re
+
+import pytest
+
+from faker import Faker
+from faker.contrib.pytest.plugin import DEFAULT_SEED
+
+LOCALE_TEST_CLASS_NAME_REGEX = re.compile(
+    r'^Test(?P<language>[A-Z][a-z]{1,2})(?P<region>[A-Z][a-z])$',
+)
+
+
+@pytest.fixture(scope='class', autouse=True)
+def _class_locale_faker(request):
+    if not request.cls:
+        return None
+    class_name = request.cls.__name__
+    match = LOCALE_TEST_CLASS_NAME_REGEX.fullmatch(class_name)
+    if not match:
+        return None
+    locale = '{language}_{region}'.format(**match.groupdict())
+    locale = pylocale.normalize(locale).split('.')[0]
+    return Faker(locale=locale)
+
+
+@pytest.fixture(autouse=True)
+def faker(_class_locale_faker, faker):
+    if not _class_locale_faker:
+        return faker
+    _class_locale_faker.seed_instance(DEFAULT_SEED)
+    return _class_locale_faker
+
+
+@pytest.fixture(scope='class', autouse=True)
+def num_samples(request):
+    try:
+        num = int(request.cls.num_samples)
+    except AttributeError:
+        num = 100
+    return num
--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,22 +1,92 @@
-import unittest
-
 from unittest.mock import patch
 
-from faker import Generator
+import pytest
+
+from faker import Faker, Generator
 
 
-class GeneratorTestCase(unittest.TestCase):
+class BarProvider:
 
-    def setUp(self):
-        self.generator = Generator()
+    def foo_formatter(self):
+        return 'barfoo'
 
-    @patch('random.getstate')
-    def test_get_random(self, mock_system_random):
-        random_instance = self.generator.random
+
+class FooProvider:
+
+    def foo_formatter(self):
+        return 'foobar'
+
+    def foo_formatter_with_arguments(self, param='', append=''):
+        return 'baz' + param + append
+
+
+@pytest.fixture(autouse=True)
+def generator():
+    generator = Generator()
+    generator.add_provider(FooProvider())
+    return generator
+
+
+class TestGenerator:
+    """Test Generator class"""
+    def test_get_formatter_returns_correct_formatter(self, generator):
+        foo_provider = generator.providers[0]
+        formatter = generator.get_formatter('foo_formatter')
+        assert callable(formatter) and formatter == foo_provider.foo_formatter
+
+    def test_get_formatter_with_unknown_formatter(self, generator):
+        with pytest.raises(AttributeError) as excinfo:
+            generator.get_formatter('barFormatter')
+        assert str(excinfo.value) == 'Unknown formatter "barFormatter"'
+
+        fake = Faker('it_IT')
+        with pytest.raises(AttributeError) as excinfo:
+            fake.get_formatter('barFormatter')
+        assert str(excinfo.value) == 'Unknown formatter "barFormatter" with locale "it_IT"'
+
+    def test_format_calls_formatter_on_provider(self, generator):
+        assert generator.format('foo_formatter') == 'foobar'
+
+    def test_format_passes_arguments_to_formatter(self, generator):
+        result = generator.format('foo_formatter_with_arguments', 'foo', append='!')
+        assert result == 'bazfoo!'
+
+    def test_add_provider_overrides_old_provider(self, generator):
+        assert generator.format('foo_formatter') == 'foobar'
+        generator.add_provider(BarProvider())
+        assert generator.format('foo_formatter') == 'barfoo'
+
+    def test_parse_without_formatter_tokens(self, generator):
+        assert generator.parse('fooBar#?') == 'fooBar#?'
+
+    def test_parse_with_valid_formatter_tokens(self, generator):
+        result = generator.parse('This is {{foo_formatter}} a text with "{{ foo_formatter }}"')
+        assert result == 'This is foobar a text with " foobar "'
+
+    def test_parse_with_unknown_formatter_token(self, generator):
+        with pytest.raises(AttributeError) as excinfo:
+            generator.parse('{{barFormatter}}')
+        assert str(excinfo.value) == 'Unknown formatter "barFormatter"'
+
+    def test_magic_call_calls_format(self, generator):
+        assert generator.foo_formatter() == 'foobar'
+
+    def test_magic_call_calls_format_with_arguments(self, generator):
+        assert generator.foo_formatter_with_arguments('foo') == 'bazfoo'
+
+    @patch('faker.generator.random_module.getstate')
+    def test_get_random(self, mock_system_random, generator):
+        random_instance = generator.random
         random_instance.getstate()
-        assert not mock_system_random.called
+        mock_system_random.assert_not_called()
 
-    @patch('random.seed')
-    def test_random_seed_doesnt_seed_system_random(self, mock_system_random):
-        self.generator.seed(0)
-        assert not mock_system_random.called
+    @patch('faker.generator.random_module.seed')
+    def test_random_seed_doesnt_seed_system_random(self, mock_system_random, generator):
+        # Save original state of shared random instance to avoid affecting other tests
+        state = generator.random.getstate()
+
+        generator.seed(0)
+        mock_system_random.assert_not_called()
+
+        # Restore state of shared random instance
+        generator.random.setstate(state)
