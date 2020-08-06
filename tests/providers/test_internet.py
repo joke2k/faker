948f8576651ab3446f8130edf1a5df0ea8711392
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-08-06 16:30:30 +0200

    Add es_ES internet provider. (#1203)

--- tests/providers/test_internet.py
+++ tests/providers/test_internet.py
@@ -11,6 +11,7 @@ from validators import email as validate_email
 
 from faker.providers.internet import Provider as InternetProvider
 from faker.providers.internet.en_GB import Provider as EnGbInternetProvider
+from faker.providers.internet.es_ES import Provider as EsEsInternetProvider
 from faker.providers.internet.pl_PL import Provider as PlPlInternetProvider
 from faker.providers.internet.zh_CN import Provider as ZhCnInternetProvider
 from faker.providers.person.ja_JP import Provider as JaPersonProvider
@@ -642,3 +643,11 @@ class TestEnGb:
     def test_tld(self, faker):
         tld = faker.tld()
         assert tld in EnGbInternetProvider.tlds
+
+
+class TestEsEs:
+    """Tests for the es_ES locale."""
+
+    def test_tld(self, faker):
+        tld = faker.tld()
+        assert tld in EsEsInternetProvider.tlds
