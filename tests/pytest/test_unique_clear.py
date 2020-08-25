62df226f25b57ada990aa52f7424c7a05b492c18
Jack Edge <yellowbounder@gmail.com>
2020-08-25 17:26:16 +0100

    🚨 Add test for unique fixture clearing
    Add a test for checking that using the `faker` fixture across multiple
    functions have their record of pre-existing generated unique values
    cleared.

---
+++ tests/pytest/test_unique_clear.py
@@ -0,0 +1,18 @@
+def test_unique_clears(testdir):
+    """Successive uses of the `faker` pytest fixture have the
+    generated unique values cleared between functions."""
+
+    testdir.makepyfile(
+        """
+        def test_generate_two_unique_booleans(faker):
+            faker.unique.boolean()
+            faker.unique.boolean()
+
+        def test_another_boolean(faker):
+            faker.unique.boolean()
+        """
+    )
+
+    result = testdir.runpytest()
+
+    result.assert_outcomes(passed=2)
