4fcf7a2d4b7884aad0ec5030727ec6ad1e4a132b
Austin Pua <pua.austin.anderson@gmail.com>
2020-08-27 21:48:33 +0800

    Pytestify and improve color provider tests

--- tests/providers/test_color.py
+++ tests/providers/test_color.py
@@ -1,147 +1,139 @@
 import copy
 import random
 import re
-import unittest
 
-from re import search
+import pytest
 
-from faker import Faker
 from faker.providers.color import RandomColor
-from faker.providers.color.es_ES import Provider as EsEsProvider
-from faker.providers.color.fa_IR import Provider as FaIrProvider
-from faker.providers.color.hy_AM import Provider as HyAmProvider
+from faker.providers.color.es_ES import Provider as EsEsColorProvider
+from faker.providers.color.fa_IR import Provider as FaIrColorProvider
+from faker.providers.color.hy_AM import Provider as HyAmColorProvider
 
 
-class TestColor(unittest.TestCase):
+class TestColorProvider:
+    """Test color provider methods"""
+    num_samples = 10000
 
-    def setUp(self):
-        self.fake = Faker('en_US')
-        Faker.seed(0)
+    def test_safe_hex_color(self, faker, num_samples):
+        assert all(
+            re.fullmatch(r'#(?:([0-9a-f])\1){3}', faker.safe_hex_color())
+            for _ in range(num_samples)
+        )
 
-    def test_safe_hex_color(self):
-        assert all(search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', self.fake.safe_hex_color()) for _ in range(1000))
+    def test_hex_color(self, faker, num_samples):
+        assert all(
+            re.fullmatch(r'#[0-9a-f]{6}', faker.hex_color())
+            for _ in range(num_samples)
+        )
 
-    def test_hex_color(self):
-        assert all(search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', self.fake.hex_color()) for _ in range(1000))
+    def test_rgb_color(self, faker, num_samples):
+        for _ in range(num_samples):
+            r, g, b = list(map(int, faker.rgb_color().split(',')))
+            assert 0 <= r <= 255
+            assert 0 <= g <= 255
+            assert 0 <= b <= 255
 
-    def test_rgb_color(self):
-        maxval = 0
-        minval = 0
+    def test_rgb_css_color(self, faker, num_samples):
+        pattern = re.compile(r'rgb\((?P<rgb>\d{1,3},\d{1,3},\d{1,3})\)')
+        for _ in range(num_samples):
+            match = pattern.fullmatch(faker.rgb_css_color())
+            rgb = match.group('rgb')
+            r, g, b = list(map(int, rgb.split(',')))
+            assert 0 <= r <= 255
+            assert 0 <= g <= 255
+            assert 0 <= b <= 255
 
-        for _ in range(1000):
-            current = list(map(int, self.fake.rgb_color().split(',')))
-            if max(current) > maxval:
-                maxval = max(current)
-            if min(current) > minval:
-                minval = min(current)
-
-        assert maxval <= 255
-        assert minval >= 0
-
-    def test_rgb_css_color(self):
-        maxval = 0
-        minval = 0
-
-        for _ in range(1000):
-            current = list(map(int, self.fake.rgb_css_color()[4:-1].split(',')))
-            if max(current) > maxval:
-                maxval = max(current)
-            if min(current) > minval:
-                minval = min(current)
-
-        assert maxval <= 255
-        assert minval >= 0
-
-    def test_color(self):
+    def test_color(self, faker, num_samples):
         baseline_random_color = RandomColor(seed=4761)
-        expected = [baseline_random_color.generate() for _ in range(10000)]
+        expected = [baseline_random_color.generate() for _ in range(num_samples)]
 
         # The `color` provider method should behave like the `generate`
         # method of a standalone RandomColor instance for a given seed
-        Faker.seed(4761)
-        colors = [self.fake.color() for _ in range(10000)]
+        faker.seed_instance(4761)
+        colors = [faker.color() for _ in range(num_samples)]
         assert colors == expected
 
 
-class TestRandomColor(unittest.TestCase):
-
+class TestRandomColor:
+    """Test RandomColor class"""
+    num_samples = 1000
     seed = 4761
     hsv_color_pattern = re.compile(
-        r'^hsv\('
+        r'hsv\('
         r'(?P<h>\d|[1-9]\d|[1-3]\d{2}), '
         r'(?P<s>\d|[1-9]\d|100), '
-        r'(?P<v>\d|[1-9]\d|100)\)$',
+        r'(?P<v>\d|[1-9]\d|100)\)',
     )
     hsl_color_pattern = re.compile(
-        r'^hsl\('
+        r'hsl\('
         r'(?P<h>\d|[1-9]\d|[1-3]\d{2}), '
         r'(?P<s>\d|[1-9]\d|[1-3]\d{2}), '
-        r'(?P<l>\d|[1-9]\d|[1-3]\d{2})\)$',
+        r'(?P<l>\d|[1-9]\d|[1-3]\d{2})\)',
     )
     rgb_color_pattern = re.compile(
-        r'^rgb\('
+        r'rgb\('
         r'(?P<r>\d|[1-9]\d|[1-3]\d{2}), '
         r'(?P<g>\d|[1-9]\d|[1-3]\d{2}), '
-        r'(?P<b>\d|[1-9]\d|[1-3]\d{2})\)$',
+        r'(?P<b>\d|[1-9]\d|[1-3]\d{2})\)',
     )
-    hex_color_pattern = re.compile(r'^#[0-9a-f]{6}$')
+    hex_color_pattern = re.compile(r'#[0-9a-f]{6}')
 
-    def setUp(self):
+    def setup_method(self):
         self.random_color = RandomColor(seed=self.seed)
 
-    def test_color_format_hsv(self):
-        for _ in range(10000):
+    def test_color_format_hsv(self, num_samples):
+        for _ in range(num_samples):
             hsv_color = self.random_color.generate(color_format='hsv')
-            match = self.hsv_color_pattern.match(hsv_color)
+            match = self.hsv_color_pattern.fullmatch(hsv_color)
             assert match
             groupdict = match.groupdict()
             assert 0 <= int(groupdict['h']) <= 360
             assert 0 <= int(groupdict['s']) <= 100
             assert 0 <= int(groupdict['v']) <= 100
 
-    def test_color_format_hsl(self):
-        for _ in range(10000):
+    def test_color_format_hsl(self, num_samples):
+        for _ in range(num_samples):
             hsl_color = self.random_color.generate(color_format='hsl')
-            match = self.hsl_color_pattern.match(hsl_color)
+            match = self.hsl_color_pattern.fullmatch(hsl_color)
             assert match
             groupdict = match.groupdict()
             assert 0 <= int(groupdict['h']) <= 360
             assert 0 <= int(groupdict['s']) <= 100
             assert 0 <= int(groupdict['l']) <= 100
 
-    def test_color_format_rgb(self):
-        for _ in range(10000):
+    def test_color_format_rgb(self, num_samples):
+        for _ in range(num_samples):
             rgb_color = self.random_color.generate(color_format='rgb')
-            match = self.rgb_color_pattern.match(rgb_color)
+            match = self.rgb_color_pattern.fullmatch(rgb_color)
             assert match
             groupdict = match.groupdict()
             assert 0 <= int(groupdict['r']) <= 255
             assert 0 <= int(groupdict['g']) <= 255
             assert 0 <= int(groupdict['b']) <= 255
 
-    def test_color_format_hex(self):
-        for _ in range(10000):
+    def test_color_format_hex(self, num_samples):
+        for _ in range(num_samples):
             hex_color = self.random_color.generate(color_format='hex')
-            assert self.hex_color_pattern.match(hex_color)
+            assert self.hex_color_pattern.fullmatch(hex_color)
 
-    def test_color_format_unspecified(self):
-        for _ in range(10000):
+    def test_color_format_unspecified(self, num_samples):
+        for _ in range(num_samples):
             color = self.random_color.generate()
-            assert self.hex_color_pattern.match(color)
+            assert self.hex_color_pattern.fullmatch(color)
 
     def test_hue_integer(self):
         # HSV format is used, because whatever hue value supplied must be present in the output
         for hue in range(360):
             colors = [self.random_color.generate(hue=hue, color_format='hsv') for _ in range(10)]
             for color in colors:
-                match = self.hsv_color_pattern.match(color)
+                match = self.hsv_color_pattern.fullmatch(color)
                 assert match
                 groupdict = match.groupdict()
                 assert int(groupdict['h']) == hue
 
-    def test_hue_float(self):
+    def test_hue_float(self, num_samples):
         baseline_random_color = RandomColor(seed=self.seed)
-        for _ in range(1000):
+        for _ in range(num_samples):
             hue_float = random.uniform(0, 360)
             hue_int = int(hue_float)
             expected = [baseline_random_color.generate(hue=hue_int) for _ in range(10)]
@@ -184,22 +176,22 @@ class TestRandomColor(unittest.TestCase):
         colors = [self.random_color.generate(hue='pink') for _ in range(5)]
         assert colors == expected
 
-    def test_hue_tuple_beyond_limits(self):
+    def test_hue_tuple_beyond_limits(self, num_samples):
         baseline_random_color = RandomColor(seed=self.seed)
-        expected = [baseline_random_color.generate(hue=[0, 360]) for _ in range(1000)]
+        expected = [baseline_random_color.generate(hue=[0, 360]) for _ in range(num_samples)]
 
         # Using a tuple with values not between 0 and 360 should yield the same results
         # as using a tuple with clamped values for a given seed
-        colors = [self.random_color.generate(hue=[-100, 4500]) for _ in range(1000)]
+        colors = [self.random_color.generate(hue=[-100, 4500]) for _ in range(num_samples)]
         assert colors == expected
 
-    def test_hue_tuple_inverted_values(self):
+    def test_hue_tuple_inverted_values(self, num_samples):
         baseline_random_color = RandomColor(seed=self.seed)
-        expected = [baseline_random_color.generate(hue=[45, 75]) for _ in range(1000)]
+        expected = [baseline_random_color.generate(hue=[45, 75]) for _ in range(num_samples)]
 
         # Using a tuple with inverted values should yield the same results
         # as using the correctly ordered tuple for a given seed
-        colors = [self.random_color.generate(hue=[75, 45]) for _ in range(1000)]
+        colors = [self.random_color.generate(hue=[75, 45]) for _ in range(num_samples)]
         assert colors == expected
 
     def test_hue_invalid(self):
@@ -213,7 +205,7 @@ class TestRandomColor(unittest.TestCase):
         ]
 
         for invalid_value in invalid_values:
-            with self.assertRaises(TypeError):
+            with pytest.raises(TypeError):
                 self.random_color.generate(hue=invalid_value)
 
     def test_luminosity_word(self):
@@ -233,11 +225,11 @@ class TestRandomColor(unittest.TestCase):
         colors = [self.random_color.generate(luminosity='random') for _ in range(5)]
         assert colors == expected
 
-    def test_luminosity_invalid(self):
+    def test_luminosity_invalid(self, num_samples):
         baseline_random_color = RandomColor(seed=self.seed)
-        expected = [baseline_random_color.generate() for _ in range(1000)]
+        expected = [baseline_random_color.generate() for _ in range(num_samples)]
 
-        colors = [self.random_color.generate(luminosity='invalid_value') for _ in range(1000)]
+        colors = [self.random_color.generate(luminosity='invalid_value') for _ in range(num_samples)]
         assert colors == expected
 
     def test_bad_color_map(self):
@@ -248,59 +240,53 @@ class TestRandomColor(unittest.TestCase):
         colormap = copy.deepcopy(self.random_color.colormap)
         colormap['yellow']['hue_range'] = [47, 61]
         self.random_color.colormap = colormap
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             self.random_color.generate(hue=62)
 
 
-class TestHyAM(unittest.TestCase):
-    """ Tests colors in the hy_AM locale """
+class TestHyAm:
+    """Test hy_AM color provider methods"""
 
-    def setUp(self):
-        self.fake = Faker('hy_AM')
-        Faker.seed(0)
+    def test_color_name(self, faker, num_samples):
+        for _ in range(num_samples):
+            color_name = faker.color_name()
+            assert isinstance(color_name, str)
+            assert color_name in HyAmColorProvider.all_colors.keys()
 
-    def test_color_name(self):
-        color_name = self.fake.color_name()
-        assert isinstance(color_name, str)
-        assert color_name in HyAmProvider.all_colors.keys()
-
-    def test_safe_color_name(self):
-        safe_color_name = self.fake.safe_color_name()
-        assert isinstance(safe_color_name, str)
-        assert safe_color_name in HyAmProvider.safe_colors
+    def test_safe_color_name(self, faker, num_samples):
+        for _ in range(num_samples):
+            safe_color_name = faker.safe_color_name()
+            assert isinstance(safe_color_name, str)
+            assert safe_color_name in HyAmColorProvider.safe_colors
 
 
-class TestFaIr(unittest.TestCase):
-    """ Tests colors in the fa_IR locale """
+class TestFaIr:
+    """Test fa_IR color provider methods"""
 
-    def setUp(self):
-        self.fake = Faker('fa_IR')
-        Faker.seed(0)
+    def test_color_name(self, faker, num_samples):
+        for _ in range(num_samples):
+            color_name = faker.color_name()
+            assert isinstance(color_name, str)
+            assert color_name in FaIrColorProvider.all_colors.keys()
 
-    def test_color_name(self):
-        color_name = self.fake.color_name()
-        assert isinstance(color_name, str)
-        assert color_name in FaIrProvider.all_colors.keys()
-
-    def test_safe_color_name(self):
-        safe_color_name = self.fake.safe_color_name()
-        assert isinstance(safe_color_name, str)
-        assert safe_color_name in FaIrProvider.safe_colors
+    def test_safe_color_name(self, faker, num_samples):
+        for _ in range(num_samples):
+            safe_color_name = faker.safe_color_name()
+            assert isinstance(safe_color_name, str)
+            assert safe_color_name in FaIrColorProvider.safe_colors
 
 
-class TestEsES(unittest.TestCase):
-    """ Tests colors in the es_ES locale """
+class TestEsEs:
+    """Test es_ES color provider methods"""
 
-    def setUp(self):
-        self.fake = Faker('es_ES')
-        Faker.seed(0)
+    def test_color_name(self, faker, num_samples):
+        for _ in range(num_samples):
+            color_name = faker.color_name()
+            assert isinstance(color_name, str)
+            assert color_name in EsEsColorProvider.all_colors.keys()
 
-    def test_color_name(self):
-        color_name = self.fake.color_name()
-        assert isinstance(color_name, str)
-        assert color_name in EsEsProvider.all_colors.keys()
-
-    def test_safe_color_name(self):
-        safe_color_name = self.fake.safe_color_name()
-        assert isinstance(safe_color_name, str)
-        assert safe_color_name in EsEsProvider.safe_colors
+    def test_safe_color_name(self, faker, num_samples):
+        for _ in range(num_samples):
+            safe_color_name = faker.safe_color_name()
+            assert isinstance(safe_color_name, str)
+            assert safe_color_name in EsEsColorProvider.safe_colors
