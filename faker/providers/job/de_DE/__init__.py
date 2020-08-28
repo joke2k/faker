825b06954c850ea55cbae1517075956960bd0ff6
datadominik <dominik.schniertshauer@googlemail.com>
2020-08-28 11:33:37 +0800

    Fixed typo in German job provider

--- faker/providers/job/de_DE/__init__.py
+++ faker/providers/job/de_DE/__init__.py
@@ -24,7 +24,7 @@ class Provider(BaseProvider):
         'Fachinformatiker',
         'Fleischer',
         'Florist',
-        'Forstwirst',
+        'Forstwirt',
         'Friseur',
         'Informatiker',
         'Programmierer',
