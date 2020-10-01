joke2k/faker

commit d5ad2d04dd512852bd2903f12d991db6de2d21dc

Added provider for safe colours under hu_HU.

faker/providers/color/hu_HU/__init__.py
@@ -1,0 +1,14 @@
+# coding=utf-8
+from collections import OrderedDict
+
+from faker.providers import BaseProvider
+
+
+class Provider(BaseProvider):
+
+    safe_colors = (
+        'fekete', 'bordó', 'zöld', 'királykék', 'oliva',
+        'bíbor', 'kékeszöld', 'citromzöld', 'kék', 'ezüst',
+        'szürke', 'sárga', 'mályva', 'akvamarin', 'fehér',
+    )
+

commit 46172d9cefa580fd22696d90fa339ef0352e5104

Fix hu_HU color provider inheritance
It was broken since it was first introduced
in d5ad2d04dd512852bd2903f12d991db6de2d21dc

faker/providers/color/hu_HU/__init__.py
@@ -1,7 +1,7 @@
-from faker.providers import BaseProvider
+from .. import Provider as ColorProvider
 
 
-class Provider(BaseProvider):
+class Provider(ColorProvider):
     """Implement color provider for ``hu_HU`` locale."""
 
     safe_colors = (