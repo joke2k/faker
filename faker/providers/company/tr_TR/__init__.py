369e1771eca4aa4055df8cf701c9e0ace8d3c7e2
Veli EROGLU <realveli24@gmail.com>
2020-10-02 23:29:14 +0300

    Added Company Provider for ``tr_TR`` locale. (#1284)
    * Added Company Provider for tr_TR locale.
    
    * added trailing commas & newline at end of file
    
    * large company variable name rework
    
    * large_company renamed to large_companies
    
    * TestTrTr large company test reworked
    
    * split test_company imports into groups
    
    * rework splitting test_company imports
    
    * import sorting rework
    
    * comma trails added.

---
+++ faker/providers/company/tr_TR/__init__.py
@@ -0,0 +1,25 @@
+from .. import Provider as CompanyProvider
+
+
+class Provider(CompanyProvider):
+    formats = (
+        '{{last_name}} {{company_suffix}}',
+        '{{last_name}} {{last_name}} {{company_suffix}}',
+        '{{large_company}}',
+    )
+    # Source: https://www.capital.com.tr/listeler/capital-500
+    large_companies = (
+        'Tüpraş', 'Türk Hava Yolları', 'Petrol Ofisi', 'Opet', 'BİM', 'Ford Otosan', 'Arçelik', 'Vestel',
+        'Toyota Otomotiv', 'Oyak Renault', 'A101 Yeni Mağzacılık', 'Turkcell', 'Türk Telekom', 'Anadolu Efes', 'Migros',
+        'LC Waikiki', 'Peak Games', 'Masomo', 'EnerjiSA', 'Tofaş', 'Şişecam', 'Selçuk Ecza', 'ŞOK Marketler', 'Petkim',
+        'Limak İnşaat', 'Aselsan', 'Havelsan', 'Roketsan', 'Şişecam',
+    )
+    company_suffixes = (
+        'A.Ş.', 'Ltd.', 'Şti.',
+    )
+
+    def large_company(self):
+        """
+        :example: 'Peak Games'
+        """
+        return self.random_element(self.large_companies)
