02ee49ed23d06f57143ee1366a31465fa3d86c4d
andreypetrenko82 <silverio100182@gmail.com>
2020-10-23 01:50:19 +0300

    Update __init__.py
    Please add nationalities in English variant

--- faker/providers/geo/en_US/__init__.py
+++ faker/providers/geo/en_US/__init__.py
@@ -1,5 +1,27 @@
-from .. import Provider as BaseProvider
+from .. import Provider as GeoProvider
 
 
-class Provider(BaseProvider):
-    pass
+class Provider(GeoProvider):
+    
+    nationalities = (
+        "Afghan", "Albanian", "Algerian", "Argentine, Argentinian", "Australian", "Austrian", "Bangladeshi", "Belgian",
+        "Bolivian", "Batswana", "Brazilian", "Bulgarian", "Cambodian", "Cameroonian", "Canadian", "Chilean", "Chinese",
+        "Colombian", "Costa Rican", "Croatian", "Cuban", "Czech", "Danish", "Dominican", "Ecuadorian", "Egyptian",
+        "Salvadorian", "English", "Estonian", "Ethiopian", "Fijian", "Finnish", "French", "German", "Ghanaian", "Greek",
+        "Guatemalan", "Haitian", "Honduran", "Hungarian", "Icelandic", "Indian", "Indonesian", "Iranian", "Iraqi",
+        "Irish", "Israeli", "Italian", "Jamaican", "Japanese", "Jordanian", "Kenyan", "Kuwaiti", "Lao", "Latvian",
+        "Lebanese", "Libyan", "Lithuanian", "Malagasy", "Malaysian", "Malian", "Maltese", "Mexican", "Mongolian",
+        "Moroccan", "Mozambican", "Namibian", "Nepalese", "Dutch", "New Zealand", "Nicaraguan", "Nigerian", "Norwegian",
+        "Pakistani", "Panamanian", "Paraguayan", "Peruvian", "Philippine", "Polish", "Portuguese", "Romanian",
+        "Russian", "Saudi", "Scottish", "Senegalese", "Serbian", "Singaporean", "Slovak", "South African", "Korean",
+        "Spanish", "Sri Lankan", "Sudanese", "Swedish", "Swiss", "Syrian", "Taiwanese", "Tajikistani", "Thai", "Tongan",
+        "Tunisian", "Turkish", "Ukrainian", "Emirati", "British", "American", "Uruguayan", "Venezuelan", "Vietnamese",
+        "Welsh", "Zambian", "Zimbabwean",
+    )
+        
+        
+    def nationality(self):
+        """
+        :example 'American'
+        """
+        return self.random_element(self.nationalities)
