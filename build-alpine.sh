cdfa2534ab5c3e49c4f184ee5b8b54f648c5c8fd
Arthit Suriyawongkul <arthit@gmail.com>
2020-12-07 22:31:14 +0700

    Add Alpine test environment to tox (#1337)
    * Add testenv py39 and alpine. Remove py35
    
    * Add TEST_ALPINE environment variable to .travis.yml
    
    * Update README.rst

---
+++ build-alpine.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ -z "${TEST_ALPINE}" ]]; then
+    echo "Not on Travis"
+    exit 0
+fi
+
+docker run -v ${PWD}:/code -e INSTALL_REQUIREMENTS=${INSTALL_REQUIREMENTS} python:3-alpine sh -c "
+    apk update \
+    && apk add git \
+    && pip install tox coveralls \
+    && export LANG='en_US.UTF-8' \
+    && cd /code \
+    && tox -e py\
+    && coverage report"
