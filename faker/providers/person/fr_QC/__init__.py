0978a5081d82ff02bb9d27083cce9db3b988e669
Paul Prescod <pprescod@salesforce.com>
2021-02-09 08:40:09 -0800

    Formal deprecation of fr_QC

--- faker/providers/person/fr_QC/__init__.py
+++ faker/providers/person/fr_QC/__init__.py
@@ -1,3 +1,9 @@
-from ..fr_CA import *
+import warnings
 
-# This locale is deprecated. Please use fr_CA instead.
+from ..fr_CA import Provider as FRCAProvider
+
+
+class Provider(FRCAProvider):
+    def __init__(self, *args, **kwargs):
+        warnings.warn("fr_QC locale is deprecated. Please use fr_CA.")
+        super().__init__(*args, **kwargs)
