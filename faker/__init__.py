9929afc9c9fd4bd75f2ad4b7eb9c132e67e66ce8
fcurella <flavio.curella@gmail.com>
2021-03-15 09:18:42 -0500

    Bump version: 6.6.0 → 6.6.1

--- faker/__init__.py
+++ faker/__init__.py
@@ -2,6 +2,6 @@ from faker.factory import Factory
 from faker.generator import Generator
 from faker.proxy import Faker
 
-VERSION = '6.6.0'
+VERSION = '6.6.1'
 
 __all__ = ('Factory', 'Generator', 'Faker')
