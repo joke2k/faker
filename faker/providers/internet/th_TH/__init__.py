b311af09e533d23cb5f33a04d327fd47a83ba36f
Arthit Suriyawongkul <arthit@gmail.com>
2021-04-22 13:26:03 +0700

    Add internet user_name_formats and tlds for th_TH

---
+++ faker/providers/internet/th_TH/__init__.py
@@ -0,0 +1,28 @@
+from collections import OrderedDict
+
+from faker.utils.decorators import slugify
+
+from .. import Provider as InternetProvider
+
+
+class Provider(InternetProvider):
+    user_name_formats = (
+        '{{last_romanized_name}}.{{first_romanized_name}}',
+        '{{first_romanized_name}}.{{last_romanized_name}}',
+        '{{first_romanized_name}}##',
+        '?{{last_romanized_name}}',
+    )
+
+    tlds = OrderedDict((
+        ('in.th', 100),
+        ('co.th', 80),
+        ('go.th', 40),
+        ('or.th', 40),
+        ('ac.th', 20),
+        ('net.th', 10),
+        ('mi.th', 5),
+        ('co', 10),
+        ('net', 20),
+        ('com', 150),
+        ('org', 50),
+    ))
