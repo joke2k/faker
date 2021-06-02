ca43530c384807393bf2812ea86a16978738b2af
fcurella <flavio.curella@gmail.com>
2021-06-02 10:46:03 -0500

    add os reqs to alpine build

--- build-alpine.sh
+++ build-alpine.sh
@@ -7,7 +7,7 @@ fi
 
 docker run -v ${PWD}:/code -e INSTALL_REQUIREMENTS=${INSTALL_REQUIREMENTS} python:3-alpine sh -c "
     apk update \
-    && apk add git jpeg-dev zlib-dev \
+    && apk add git build-base jpeg-dev zlib-dev \
     && pip install tox coveralls \
     && export LANG='en_US.UTF-8' \
     && cd /code \
