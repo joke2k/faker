fb16a4c66be509cb59e8626aa9a8269775e68a1c
Arthit Suriyawongkul <arthit@gmail.com>
2021-06-10 21:05:27 +0100

    Add simple test for th_TH internet provider

--- faker/providers/internet/th_TH/__init__.py
+++ faker/providers/internet/th_TH/__init__.py
@@ -4,16 +4,29 @@ from .. import Provider as InternetProvider
 
 
 class Provider(InternetProvider):
-    tlds = OrderedDict((
-        ('in.th', 100),
-        ('co.th', 80),
-        ('go.th', 40),
-        ('or.th', 40),
-        ('ac.th', 20),
-        ('net.th', 10),
-        ('mi.th', 5),
-        ('co', 10),
-        ('net', 20),
-        ('com', 150),
-        ('org', 50),
-    ))
+    free_email_domains = (
+        "hotmail.com",
+        "gmail.com",
+        "outlook.com",
+        "yahoo.com",
+        "ymail.com",
+        "kon.in.th",
+        "icloud.com",
+        "protonmail.com",
+    )
+
+    tlds = OrderedDict(
+        (
+            ("in.th", 100),
+            ("co.th", 80),
+            ("go.th", 40),
+            ("or.th", 40),
+            ("ac.th", 20),
+            ("net.th", 10),
+            ("mi.th", 5),
+            ("co", 10),
+            ("net", 20),
+            ("com", 150),
+            ("org", 50),
+        )
+    )
