dc2c301449f45ea42e7935156c7030f718a6260c
jabertuhin <jabertuhin@gmail.com>
2021-09-24 18:11:35 +0600

    Add datetime provider for ``bn_BD`` (#1518)
    * Add bn_bd package in date_time with test.
    
    * Remove extra trailing spaces.

---
+++ faker/providers/date_time/bn_BD/__init__.py
@@ -0,0 +1,37 @@
+from .. import Provider as DateTimeProvider
+
+
+class Provider(DateTimeProvider):
+
+    DAY_NAMES = {
+        "0": "রবিবার",
+        "1": "সোমবার",
+        "2": "মঙ্গলবার",
+        "3": "বুধবার",
+        "4": "বৃহস্পতিবার",
+        "5": "শুক্রবার",
+        "6": "শনিবার",
+    }
+
+    MONTH_NAMES = {
+        "01": "জানুয়ারি",
+        "02": "ফেব্রুয়ারি",
+        "03": "মার্চ",
+        "04": "এপ্রিল",
+        "05": "মে",
+        "06": "জুন",
+        "07": "জুলাই",
+        "08": "আগস্ট",
+        "09": "সেপ্টেম্বর",
+        "10": "অক্টোবর",
+        "11": "নভেম্বর",
+        "12": "ডিসেম্বর",
+    }
+
+    def day_of_week(self):
+        day = self.date("%w")
+        return self.DAY_NAMES[day]
+
+    def month_name(self):
+        month = self.month()
+        return self.MONTH_NAMES[month]
