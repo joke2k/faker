28f7daaae7cfbd507e7e56c0a485bc55cd5dcc69
Marcel Robeer <marcel_robeer@hotmail.com>
2021-10-10 23:41:20 +0200

    OrderedDict Python 3.6 fix

--- faker/typing.py
+++ faker/typing.py
@@ -1,5 +1,11 @@
 from datetime import date, datetime, timedelta
-from typing import Any, Dict, OrderedDict, Sequence, Union
+from typing import Any, Dict, Sequence, Union
+
+try:
+    from typing import OrderedDict
+except ImportError:
+    from typing import MutableMapping
+    OrderedDict = MutableMapping
 
 DateTime = Union[datetime, date, timedelta, str]
 Elements = Union[Sequence[Any], Dict[str, Any], OrderedDict[str, Any]]
