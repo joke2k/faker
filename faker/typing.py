fc1a851b5300257915db953b49d37bff0b7c9cac
Marcel Robeer <marcel_robeer@hotmail.com>
2021-10-11 18:07:55 +0200

    Typo

--- faker/typing.py
+++ faker/typing.py
@@ -5,9 +5,9 @@ from typing import Any, Dict, Sequence, Union
 
 major, minor, patchlevel = (int(version) for version in platform.python_version_tuple())
 
-if major >= 3 or major == 3 and minor >= 10:
+if major > 3 or major == 3 and minor >= 10:  # Python 3.10 and above
     from collections import OrderedDict
-elif major == 3 and (minor < 7 or minor == 7 and patchlevel < 2):
+elif major == 3 and (minor < 7 or minor == 7 and patchlevel < 2):  # Python 3.7.1 and below
     from typing import MutableMapping as OrderedDict
 else:
     from typing import OrderedDict
