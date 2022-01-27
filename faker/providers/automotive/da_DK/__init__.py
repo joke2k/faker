324dc1b67c9984a7b43fd0b35f89ab2ae02d29d1
epetrovski <erik@petrovski.dk>
2022-01-27 22:04:10 +0100

    Add danish license plate format

---
+++ faker/providers/automotive/da_DK/__init__.py
@@ -0,0 +1,14 @@
+from .. import Provider as AutomotiveProvider
+
+
+class Provider(AutomotiveProvider):
+    """Implement automotive provider for ``da_DK`` locale.
+
+    Sources:
+
+    - https://en.wikipedia.org/wiki/Vehicle_registration_plates_of_Denmark
+    """
+
+    license_formats = (
+        "?? ## ###",
+    )
