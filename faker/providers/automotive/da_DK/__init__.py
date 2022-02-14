8200972e50a95949cb9f4eb08b19400a79f0387a
epetrovski <erik@petrovski.dk>
2022-02-14 21:53:36 +0100

    Fix line lenght errors. 2nd attempt.

--- faker/providers/automotive/da_DK/__init__.py
+++ faker/providers/automotive/da_DK/__init__.py
@@ -2,7 +2,7 @@ from .. import Provider as AutomotiveProvider
 
 
 class Provider(AutomotiveProvider):
-    """Implement automotive provider for ``da_DK`` locale.
+    """Implement automotive provider for ``da_DK`` locale. # NOQA
     Source: https://en.wikipedia.org/wiki/Vehicle_registration_plates_of_Denmark  # NOQA
     """
 
