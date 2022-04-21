8d0643f2da1412722c3e73a34a0328f5cc12a4f9
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2022-04-20 23:11:38 -0500

    Fix import order

--- tests/providers/test_bank.py
+++ tests/providers/test_bank.py
@@ -10,7 +10,8 @@ from faker.providers.bank.en_GB import Provider as EnGbBankProvider
 from faker.providers.bank.en_IE import Provider as EnIeBankProvider
 from faker.providers.bank.en_PH import Provider as EnPhBankProvider
 from faker.providers.bank.es_ES import Provider as EsEsBankProvider
-from faker.providers.bank.es_MX import Provider as EsMxBankProvider, is_valid_clabe
+from faker.providers.bank.es_MX import Provider as EsMxBankProvider
+from faker.providers.bank.es_MX import is_valid_clabe
 from faker.providers.bank.fi_FI import Provider as FiFiBankProvider
 from faker.providers.bank.fr_FR import Provider as FrFrBankProvider
 from faker.providers.bank.no_NO import Provider as NoNoBankProvider
