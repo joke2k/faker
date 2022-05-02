5b392ad25c82d0ca05c562dfac9c3fc7db6c33ef
Flavio Curella <89607+fcurella@users.noreply.github.com>
2022-05-02 10:28:09 -0500

    Update __init__.py

--- faker/providers/automotive/et_EE/__init__.py
+++ faker/providers/automotive/et_EE/__init__.py
@@ -9,4 +9,4 @@ class Provider(AutomotiveProvider):
     - https://en.wikipedia.org/wiki/Vehicle_registration_plates_of_Estonia
     """
 
-    license_formats = "### ???"
+    license_formats = ("### ???",)
