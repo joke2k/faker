d720ca3d225b2dcb7dec63133a3cce9734dcdb91
Quantum0 <quantum0@mail.ru>
2022-05-04 17:06:56 +0300

    Random Git Provider

---
+++ faker/providers/git/__init__.py
@@ -0,0 +1,15 @@
+import random
+
+from .. import BaseProvider
+
+localized = False
+
+
+class Provider(BaseProvider):
+    def git_short_sha(self) -> str:
+        return '%07x' % random.randrange(16**7)
+
+    def git_commit_sha(self) -> str:
+        return '%040x' % random.randrange(16**40)
+	
+	# TODO: Random git commit message (name + date + commit sha)
