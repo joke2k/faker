28aa4d5216f03c7fb05af3280b30b0fd084a073e
Augusto <augusto.raminelli>
2022-07-20 12:38:40 -0300

    test: test license format

--- faker/providers/automotive/pt_BR/__init__.py
+++ faker/providers/automotive/pt_BR/__init__.py
@@ -3,5 +3,6 @@ from .. import Provider as AutomotiveProvider
 
 class Provider(AutomotiveProvider):
     """Implement automotive provider for ``pt_BR`` locale."""
+    # New format since March 2017
 
-    license_formats = ("???-#?##",)
+    license_formats = ("???-####",)
