54bdfa517152b5091a0b7d78baac41c07e069ef8
Augusto <augusto.raminelli>
2022-07-21 17:00:16 -0300

    fix: ident

--- faker/providers/automotive/pt_BR/__init__.py
+++ faker/providers/automotive/pt_BR/__init__.py
@@ -3,4 +3,5 @@ from .. import Provider as AutomotiveProvider
 
 class Provider(AutomotiveProvider):
     """Implement automotive provider for ``pt_BR`` locale."""
+    
     license_formats = ("???-#?##",)
