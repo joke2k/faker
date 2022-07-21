5d1e927d45c841e6d7f64e488523652265957a97
Augusto <augusto.raminelli>
2022-07-21 13:37:19 -0300

    feat: change license_formats AutomoticeProvider pt_BR and test regex

--- faker/providers/automotive/pt_BR/__init__.py
+++ faker/providers/automotive/pt_BR/__init__.py
@@ -3,5 +3,4 @@ from .. import Provider as AutomotiveProvider
 
 class Provider(AutomotiveProvider):
     """Implement automotive provider for ``pt_BR`` locale."""
-    
-    license_formats = ("???-####",)
+    license_formats = ("???-#?##",)
