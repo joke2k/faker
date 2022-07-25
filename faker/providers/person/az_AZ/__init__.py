9ec9e1ae232266ef6c1b871ff9cf999fb08aa378
Thomas Metcalfe <tommetcalfe@live.com>
2022-07-25 13:30:07 +0200

    Fix az_AZ.py

--- faker/providers/person/az_AZ/__init__.py
+++ faker/providers/person/az_AZ/__init__.py
@@ -899,17 +899,31 @@ class Provider(PersonProvider):
 
     prefixes = prefixes_female + prefixes_male
 
-    def last_name_male(self):
-        return self.random_element(self.last_names_male + self.last_names_unisex, min_length, max_length)
+    def last_name_male(
+        self, min_length: Optional[int] = None, max_length: Optional[int] = None
+    ):
+        return self.random_element(
+            self.last_names_male + self.last_names_unisex, min_length, max_length
+        )
 
-    def last_name_unique_to_male(self):
+    def last_name_unique_to_male(
+        self, min_length: Optional[int] = None, max_length: Optional[int] = None
+    ):
         return self.random_element(self.last_names_male, min_length, max_length)
 
-    def last_name_female(self):
-        return self.random_element(self.last_names_female + self.last_names_unisex, min_length, max_length)
+    def last_name_female(
+        self, min_length: Optional[int] = None, max_length: Optional[int] = None
+    ):
+        return self.random_element(
+            self.last_names_female + self.last_names_unisex, min_length, max_length
+        )
 
-    def last_name_unique_to_female(self):
+    def last_name_unique_to_female(
+        self, min_length: Optional[int] = None, max_length: Optional[int] = None
+    ):
         return self.random_element(self.last_names_female, min_length, max_length)
 
-    def last_name_unisex(self):
+    def last_name_unisex(
+        self, min_length: Optional[int] = None, max_length: Optional[int] = None
+    ):
         return self.random_element(self.last_names_unisex, min_length, max_length)
