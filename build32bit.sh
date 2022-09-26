ed404bd0681f32551576184efab799dc6d453f04
אורי <uri@speedy.net>
2022-09-26 17:33:50 +0300

    Add support for Python 3.10.  Drop support for Python 3.6. (#1720)
    * Added support for Python 3.10.
    
    * Dropped support for Python 3.6.

--- build32bit.sh
+++ build32bit.sh
@@ -8,9 +8,9 @@ fi
 docker run -v ${PWD}:/code -e INSTALL_REQUIREMENTS=${INSTALL_REQUIREMENTS} i386/ubuntu bash -c "
     apt-get update \
     && DEBIAN_FRONTEND=noninteractive apt-get install -yq python3 locales python3-pip debianutils \
-    && pip3 install tox coveralls \
+    && python3 -m pip install tox coveralls \
     && locale-gen en_US.UTF-8 \
     && export LANG='en_US.UTF-8' \
     && cd /code \
-    && tox -e py \
+    && python3 -m tox -e py \
     && coverage report"
