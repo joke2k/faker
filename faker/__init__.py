582a994054f36fbaf7389554469c01bad41c9a7c
Flavio Curella <flavio.curella@gmail.com>
2022-11-04 11:27:53 -0500

    Bump version: 15.1.4 → 15.1.5

--- faker/__init__.py
+++ faker/__init__.py
@@ -2,6 +2,6 @@ from faker.factory import Factory
 from faker.generator import Generator
 from faker.proxy import Faker
 
-VERSION = "15.1.4"
+VERSION = "15.1.5"
 
 __all__ = ("Factory", "Generator", "Faker")
