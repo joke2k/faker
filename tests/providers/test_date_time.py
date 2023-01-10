2878ecd82a134ff28ebfa3f968b296020f608e5b
Flavio Curella <flavio.curella@gmail.com>
2023-01-10 10:22:36 -0600

    more tests

--- tests/providers/test_date_time.py
+++ tests/providers/test_date_time.py
@@ -36,6 +36,7 @@ from faker.providers.date_time.pt_PT import Provider as PtPtProvider
 from faker.providers.date_time.ro_RO import Provider as RoRoProvider
 from faker.providers.date_time.ru_RU import Provider as RuProvider
 from faker.providers.date_time.sk_SK import Provider as SkSkProvider
+from faker.providers.date_time.sl_SI import Provider as SlSiProvider
 from faker.providers.date_time.ta_IN import Provider as TaInProvider
 from faker.providers.date_time.tr_TR import Provider as TrTrProvider
 from faker.providers.date_time.zh_CN import Provider as ZhCnProvider
@@ -885,6 +886,20 @@ class TestSkSk(unittest.TestCase):
     def test_month(self):
         month = self.fake.month_name()
         assert month in SkSkProvider.MONTH_NAMES.values()
+
+
+class TestSlSi(unittest.TestCase):
+    def setUp(self):
+        self.fake = Faker("sl_SI")
+        Faker.seed(0)
+
+    def test_day(self):
+        day = self.fake.day_of_week()
+        assert day in SlSiProvider.DAY_NAMES.values()
+
+    def test_month(self):
+        month = self.fake.month_name()
+        assert month in SlSiProvider.MONTH_NAMES.values()
 
 
 class TestThTh(unittest.TestCase):
