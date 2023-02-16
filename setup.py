deda2d229dbf56fcaf4e8f5ea522ad82dc365551
Michal Porteš <michalportes1@gmail.com>
2023-02-17 00:34:11 +0100

    Add support for Python 3.11 (#1796)

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development :: Libraries :: Python Modules",
