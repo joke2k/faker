b617106a4108052d91e51278cf4bd5ff047ed937
Cheng-Yang Hsieh <cyang.hsieh@gmail.com>
2023-04-24 18:10:04 +0800

    Fix zh_TW ssn provider rule

--- faker/providers/ssn/zh_TW/__init__.py
+++ faker/providers/ssn/zh_TW/__init__.py
@@ -2,7 +2,42 @@ from .. import Provider as SsnProvider
 
 
 class Provider(SsnProvider):
-    ssn_formats = ("?#########",)
 
     def ssn(self) -> str:
-        return self.bothify(self.random_element(self.ssn_formats)).upper()
+
+        def _get_alphabet_weight(c) -> int:
+            """ A=10, B=11, ...., H=17,
+            I=34,
+            J=18, K=19, ..., N=22,
+            O=35,
+            P=23, Q=24, ..., V=29,
+            W=32,
+            X=30, Y=31, Z=33
+            """
+            if ord(c) < 73:  # A-H
+                return ord(c) - 55
+            if ord(c) == 73:  # I
+                return ord(c) - 39
+            if ord(c) < 79:  # J-N
+                return ord(c) - 56
+            if ord(c) == 79:  # O
+                return ord(c) - 44
+            if ord(c) < 87:  # P-V
+                return ord(c) - 57
+            if ord(c) == 87:  # W
+                return ord(c) - 55
+            if ord(c) < 90:  # X, Y
+                return ord(c) - 58
+            return ord(c) - 57  # Z
+
+        def _checksum(s):
+            res = 0
+            for i, c in enumerate(s):
+                if i == 0:
+                    res = _get_alphabet_weight(c) % 10 * 9 + _get_alphabet_weight(c) // 10
+                else:
+                    res += int(c) * (9 - i)
+            return str(10 - res % 10)
+
+        ssn_without_checksum = self.numerify(self.random_uppercase_letter() + str(self.random_int(1, 2)) + "#######")
+        return ssn_without_checksum + _checksum(ssn_without_checksum)
