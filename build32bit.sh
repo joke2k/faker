9977147d4ba797dc986f402bf414d1c53c59c5d3
Flavio Curella <flavio.curella@gmail.com>
2023-07-11 14:05:07 -0500

    fix 32bit test

--- build32bit.sh
+++ build32bit.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 if [[ -z "${TEST_32BIT}" ]]; then
-    echo "Not on Travis"
+    echo "Not on CI"
     exit 0
 fi
 
 docker run -v ${PWD}:/code -e INSTALL_REQUIREMENTS=${INSTALL_REQUIREMENTS} i386/ubuntu bash -c "
     apt-get update \
-    && DEBIAN_FRONTEND=noninteractive apt-get install -yq python3.7 locales python3-pip debianutils \
+    && DEBIAN_FRONTEND=noninteractive apt-get install -yq python3.8 locales python3-pip debianutils \
     && python3.7 -m pip install tox coveralls \
     && locale-gen en_US.UTF-8 \
     && export LANG='en_US.UTF-8' \
     && cd /code \
-    && python3.7 -m tox -e py \
+    && python3.8 -m tox -e py \
     && coverage report"
