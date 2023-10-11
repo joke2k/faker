41e979d5574bcc6528006f10c9bba6a6147d035b
Flavio Curella <flavio.curella@gmail.com>
2023-10-11 13:51:28 -0500

    Bump version: 19.9.0 → 19.9.1

--- faker/__init__.py
+++ faker/__init__.py
@@ -2,6 +2,6 @@ from faker.factory import Factory
 from faker.generator import Generator
 from faker.proxy import Faker
 
-VERSION = "19.9.0"
+VERSION = "19.9.1"
 
 __all__ = ("Factory", "Generator", "Faker")
