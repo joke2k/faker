ee86304f43166ae6f4ede597e2e587fa17c140fc
jist <95856749+george0st@users.noreply.github.com>
2023-10-23 16:33:36 +0200

    Add geo for cs_CZ

--- tests/providers/test_geo.py
+++ tests/providers/test_geo.py
@@ -79,6 +79,11 @@ class TestEnUS(unittest.TestCase):
         assert Decimal(loc[0])  # Should be able to cast first two elements of tuple to Decimal
         assert Decimal(loc[1])
 
+class TestCsCz(unittest.TestCase):
+    def setUp(self):
+        self.fake = Faker("cs_CZ")
+        Faker.seed(0)
+
 
 class TestDeAT(unittest.TestCase):
     """Tests in addresses in the de_AT locale"""
