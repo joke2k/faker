def4b9037db977af334b77d57fc286c2df1c024e
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-10-25 18:24:03 +0200

    feat: add method to check if a year is leap year

--- faker/providers/ssn/it_IT/__init__.py
+++ faker/providers/ssn/it_IT/__init__.py
@@ -8125,3 +8125,12 @@ class Provider(SsnProvider):
         Pads shorter string with the allowed char
         """
         return sequence.ljust(3, "X")
+
+    @staticmethod
+    def is_leap_year(year: int) -> bool:
+        """
+        Checks if the one given is a leap year
+        """
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+            return True
+        return False
