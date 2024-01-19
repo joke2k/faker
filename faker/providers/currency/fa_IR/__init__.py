c31a073f2c36bd03f2690970cc09199e1649d480
Flavio Curella <flavio.curella@gmail.com>
2024-01-19 10:23:04 -0600

    lint code

--- faker/providers/currency/fa_IR/__init__.py
+++ faker/providers/currency/fa_IR/__init__.py
@@ -2,7 +2,7 @@ from .. import Provider as CurrencyProvider
 
 
 class Provider(CurrencyProvider):
-    price_formats = ["###,###,000","#,###,000,000", "%,###,###,###,###", "%,###,###,###,000,000"]
+    price_formats = ["###,###,000", "#,###,000,000", "%,###,###,###,###", "%,###,###,###,000,000"]
 
     def pricetag(self) -> str:
         return self.numerify(self.random_element(self.price_formats)) + "\uFDFC"
