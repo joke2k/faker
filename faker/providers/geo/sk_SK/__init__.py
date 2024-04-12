4883824e5652c7b45ae3aed41bf9ab8364368458
jist <95856749+george0st@users.noreply.github.com>
2024-04-12 09:11:43 +0200

    Add geo for sk_SK

---
+++ faker/providers/geo/sk_SK/__init__.py
@@ -0,0 +1,17 @@
+from .. import Provider as GeoProvider
+
+
+class Provider(GeoProvider):
+    # Source:
+    #   https://latitude.to/map/sk/slovakia/cities/
+    land_coords = (
+        ("48.14816", "17.10674", "Bratislava", "SK", "Europe/Bratislava"),
+        ("48.71395", "21.25808", "Košice", "SK", "Europe/Košice"),
+        ("48.99839", "21.23393", "Prešov", "SK", "Europe/Prešov"),
+        ("48.30763", "18.08453", "Nitra", "SK", "Europe/Nitra"),
+        ("49.22315", "18.73941", "Žilina", "SK", "Europe/Žilina"),
+        ("48.73946", "19.15349", "Banská Bystrica", "SK", "Europe/Banská Bystrica"),
+        ("48.37741", "17.58723", "Trnava", "SK", "Europe/Trnava"),
+        ("49.06651", "18.92399", "Martin", "SK", "Europe/Martin"),
+        ("48.89452", "18.04436", "Trenčín", "SK", "Europe/Trenčín"),
+    )
