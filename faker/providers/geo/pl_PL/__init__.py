c6f08a1bb67e0685f5bb9d954d27efa4bd5802ec
jist <95856749+george0st@users.noreply.github.com>
2024-05-08 17:22:47 +0200

    Update __init__.py

--- faker/providers/geo/pl_PL/__init__.py
+++ faker/providers/geo/pl_PL/__init__.py
@@ -20,4 +20,10 @@ class Provider(GeoProvider):
         ("50.79646", "19.12409", "Częstochowa", "PL", "Europe/Warsaw"),
         ("50.28682", "19.10385", "Sosnowiec", "PL", "Europe/Warsaw"),
         ("51.40253", "21.14714", "Radom", "PL", "Europe/Warsaw"),
+        ("52.1934", "21.03487", "Mokotów", "PL", "Europe/Warsaw"),
+        ("53.01375", "18.59814", "Toruń", "PL", "Europe/Warsaw"),
+        ("50.87033", "20.62752", "Kielce", "PL", "Europe/Warsaw"),
+        ("50.29761", "18.67658", "Gliwice", "PL", "Europe/Warsaw"),
+        ("50.32492", "18.78576", "Zabrze", "PL", "Europe/Warsaw"),
+        ("50.34802", "18.93282", "Bytom", "PL", "Europe/Warsaw"),
     )
