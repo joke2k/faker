ed96ca737be76679dfc6ae9683355ffff3664c7e
jist <95856749+george0st@users.noreply.github.com>
2024-05-08 17:10:14 +0200

    Add geo for pl_PL

---
+++ faker/providers/geo/pl_PL/__init__.py
@@ -0,0 +1,17 @@
+from .. import Provider as GeoProvider
+
+
+class Provider(GeoProvider):
+    # Source:
+    #   https://latitude.to/map/pl/poland/cities/
+    land_coords = (
+        ("52.22977", "21.01178", "Warsaw", "PL", "Europe/Warsaw"),
+        ("51.75", "19.46667", "Łódź", "PL", "Europe/Warsaw"),
+        ("50.06143", "19.93658", "Krakow", "PL", "Europe/Warsaw"),
+        ("51.1", "17.03333", "Wrocław", "PL", "Europe/Warsaw"),
+        ("52.40692", "16.92993", "Poznań", "PL", "Europe/Warsaw"),
+        ("54.35205", "18.64637", "Gdańsk", "PL", "Europe/Warsaw"),
+        ("53.42894", "14.55302", "Szczecin", "PL", "Europe/Warsaw"),
+        ("53.1235", "18.00762", "Bydgoszcz", "PL", "Europe/Warsaw"),
+        ("51.25", "22.56667", "Lublin", "PL", "Europe/Warsaw"),
+    )
