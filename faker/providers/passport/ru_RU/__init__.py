af6f68b6dfa80fa61da23fb87d5f5d1de1ae9e2a
Abdujabbar MIRKHALIKOV <abdujabbar.mirkhalikov@prestataire.sewan.fr>
2024-08-06 15:39:22 +0500

    Small modifications on Provider.password_owner for ru_RU

--- faker/providers/passport/ru_RU/__init__.py
+++ faker/providers/passport/ru_RU/__init__.py
@@ -1,9 +1,9 @@
 from typing import Tuple
-
+from faker.typing import SexLiteral
 from .. import Provider as PassportProvider
 
 
 class Provider(PassportProvider):
-    def passport_owner(self, gender: str = "M") -> Tuple[str, str]:
+    def passport_owner(self, gender: SexLiteral = "M") -> Tuple[str, str]:
         given_name, surname = super().passport_owner(gender)
         return surname, given_name
