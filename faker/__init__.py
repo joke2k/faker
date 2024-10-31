912359fe661af1b739b4c0f1ea87a1a3e5950d01
Flavio Curella <flavio.curella@gmail.com>
2024-10-31 12:18:41 -0500

    Bump version: 30.8.1 → 30.8.2

--- faker/__init__.py
+++ faker/__init__.py
@@ -2,6 +2,6 @@ from faker.factory import Factory
 from faker.generator import Generator
 from faker.proxy import Faker
 
-VERSION = "30.8.1"
+VERSION = "30.8.2"
 
 __all__ = ("Factory", "Generator", "Faker")
