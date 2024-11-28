9058a03a9e5c13ee3e10640d43fb42d9718e8cb8
DinhHuy2010 <huy04052010@outlook.com>
2024-11-28 18:49:44 +0700

    resolved conflicts

--- faker/__init__.py
+++ faker/__init__.py
@@ -60,6 +60,6 @@ from faker.factory import Factory
 from faker.generator import Generator
 from faker.proxy import Faker
 
-VERSION = "33.0.0"
+VERSION = "33.1.0"
 
 __all__ = ("Factory", "Generator", "Faker")
