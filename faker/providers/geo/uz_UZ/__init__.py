e04d9accf8b5a6c72db06681038252c040a0cd8c
Sergey Mamadjanov <sm@drdow.org>
2024-12-16 03:20:41 +0500

    feat: adds city geodata for Uzbekistan

---
+++ faker/providers/geo/uz_UZ/__init__.py
@@ -0,0 +1,22 @@
+from .. import Provider as GeoProvider
+
+
+class Provider(GeoProvider):
+    # Source: https://time-in.ru/coordinates/uzbekistan
+    land_coords = (
+        ("41.26465", "69.21627", "Toshkent", "UZ", "Asia/Tashkent"),
+        ("39.65417", "66.95972", "Samarqand", "UZ", "Asia/Tashkent"),
+        ("39.77472", "64.42861", "Buxoro", "UZ", "Asia/Tashkent"),
+        ("41.37833", "60.36389", "Xiva", "UZ", "Asia/Tashkent"),
+        ("39.05778", "66.83444", "Shahrisabz", "UZ", "Asia/Tashkent"),
+        ("43.76833", "59.02028", "Mo‘ynoq", "UZ", "Asia/Tashkent"),
+        ("37.22417", "67.27833", "Termiz", "UZ", "Asia/Tashkent"),
+        ("42.45306", "59.61444", "Nukus", "UZ", "Asia/Tashkent"),
+        ("41.00000", "71.66667", "Namangan", "UZ", "Asia/Tashkent"),
+        ("40.78333", "72.35000", "Andijon", "UZ", "Asia/Tashkent"),
+        ("40.38333", "71.78333", "Farg‘ona", "UZ", "Asia/Tashkent"),
+        ("38.86000", "65.79722", "Qarshi", "UZ", "Asia/Tashkent"),
+        ("40.08333", "65.37917", "Navoiy", "UZ", "Asia/Tashkent"),
+        ("40.53139", "70.94250", "Qo‘qon", "UZ", "Asia/Tashkent"),
+        ("41.55333", "60.63194", "Urganch", "UZ", "Asia/Tashkent"),
+    )
