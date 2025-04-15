a126753d1419d8f1f0088944a4cfdb4d1b301b39
Daniele Faraglia <joke2k@users.noreply.github.com>
2025-04-15 19:06:54 +0100

    Add support for unique complex value types Fixes #1817 (#2203)

--- tests/test_unique.py
+++ tests/test_unique.py
@@ -60,3 +60,18 @@ class TestUniquenessClass:
 
         with pytest.raises(TypeError, match="Accessing non-functions through .unique is not supported."):
             fake.unique.locales
+
+    def test_complex_return_types_is_supported(self):
+        """The unique decorator supports complex return types
+        like the ones used in the profile provider."""
+
+        fake = Faker()
+
+        for i in range(10):
+            fake.unique.pydict()
+
+        for i in range(10):
+            fake.unique.pylist()
+
+        for i in range(10):
+            fake.unique.pyset()
