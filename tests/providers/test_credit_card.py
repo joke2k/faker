b199f5eb2473507b93c5e3411eb0ed7ecf8c15f2
Flavio Curella <flavio.curella@gmail.com>
2025-06-11 12:51:04 -0500

    fix test

--- tests/providers/test_credit_card.py
+++ tests/providers/test_credit_card.py
@@ -191,7 +191,7 @@ class TestUkUa:
             assert card_data[0] == "ПРОСТІР"
 
 
-class TestZhCnCreditCard:
+class TestZhCn:
     mastercard_pattern: Pattern = re.compile(
         r"(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}"
     )
