d75c1c445dbd3a5f8738e2b829ee4138d473048d
Limitless <ifeoladeji619@gmail.com>
2025-08-25 15:42:36 +0100

    Add Nigerian locales (yo_NG, ha_NG, ig_NG, en_NG)

---
+++ faker/providers/person/en_NG/__init__.py
@@ -0,0 +1,35 @@
+from faker.providers.person import Provider as PersonProvider
+
+class Provider(PersonProvider):
+    # Male first names
+    first_names_male = [
+        "John", "Emmanuel", "Peter", "Samuel", "David", "Michael",
+        "Joseph", "Daniel", "James", "Paul", "Gabriel", "Joshua",
+        "Philip", "Andrew", "Stephen", "Benjamin", "Mark", "Nathaniel",
+        "Simon", "Cornelius",
+    ]
+
+    # Female first names
+    first_names_female = [
+        "Mary", "Grace", "Joy", "Patience", "Elizabeth", "Victoria",
+        "Sarah", "Deborah", "Esther", "Blessing", "Charity", "Hope",
+        "Gloria", "Agnes", "Peace", "Comfort", "Juliet", "Ruth",
+        "Angela", "Faith",
+    ]
+
+    # Combined list
+    first_names = first_names_male + first_names_female
+
+    # Prefixes
+    prefixes_male = ["Mr.", "Chief", "Dr.", "Prof.", "Engr."]
+    prefixes_female = ["Mrs.", "Miss", "Dr.", "Prof.", "Lady"]
+
+    prefixes = prefixes_male + prefixes_female
+
+    # Last names
+    last_names = [
+        "Okonkwo", "Adeyemi", "Olawale", "Chukwu", "Eze",
+        "Obi", "Abiola", "Okafor", "Balogun", "Uche", "Ogunleye",
+        "Nnamani", "Adetokunbo", "Ojo", "Ekwueme", "Oshodi",
+        "Ibrahim", "Akinwale", "Obasanjo", "Oyekan",
+    ]
