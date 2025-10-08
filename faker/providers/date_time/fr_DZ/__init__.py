2be8a9b435229595c8da5a4ec0ecd186ba439558
othmane099 <othmabe99@gmail.com>
2025-10-08 08:34:52 +0100

    use pass keyword

--- faker/providers/date_time/fr_DZ/__init__.py
+++ faker/providers/date_time/fr_DZ/__init__.py
@@ -1,8 +1,5 @@
-from typing import Any
-
 from ..fr_FR import Provider as fr_FRProvider
 
 
 class Provider(fr_FRProvider):
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        super().__init__(*args, **kwargs)
+    pass
