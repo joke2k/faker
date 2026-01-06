f88401a7f711f08c4d45418033d1e71b263ce4f8
bon12-gr <ps4gwo@gmail.com>
2026-01-06 16:12:48 +0200

    Add major GR banks to el_GR bank provider

--- faker/providers/bank/el_GR/__init__.py
+++ faker/providers/bank/el_GR/__init__.py
@@ -6,3 +6,23 @@ class Provider(BankProvider):
 
     bban_format = "#######################"
     country_code = "GR"
+
+    # Major GR banks (with a head office in Greece)
+    # Source:
+    # - https://www.bankofgreece.gr/en/main-tasks/supervision/supervised-institutions
+    # Last verified: January 2026
+    banks = (
+        "Aegean Baltic Bank",
+        "Alpha Bank",        
+        "Credia Bank",
+        "Eurobank",
+        "Optima Bank",
+        "SNAPPI",
+        "Vivabank",   
+        "Εθνική Τράπεζα",
+        "Συνεταιριστική Τράπεζα Θεσσαλίας",
+        "Συνεταιριστική Τράπεζα Καρδίτσας",
+        "Συνεταιριστική Τράπεζα Χανίων",
+        "Τράπεζα Ηπείρου",
+        "Τράπεζα Πειραιώς",
+    )
