5fd0f61645ec4ec8075e9718c475633949803275
Flavio Curella <flavio.curella@gmail.com>
2026-02-06 17:17:22 -0600

    Bump version: 40.1.3 → 40.2.0

--- faker/__init__.py
+++ faker/__init__.py
@@ -2,6 +2,6 @@ from faker.factory import Factory
 from faker.generator import Generator
 from faker.proxy import Faker
 
-VERSION = "40.1.3"
+VERSION = "40.2.0"
 
 __all__ = ("Factory", "Generator", "Faker")
