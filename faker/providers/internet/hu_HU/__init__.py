fa11378f9a159a1fd89f1a976dc6bdec47b806b8
majorzoltan80 <major.zoltan80@gmail.com>
2026-02-23 21:15:01 +0100

    Add free hungarian email domains

--- faker/providers/internet/hu_HU/__init__.py
+++ faker/providers/internet/hu_HU/__init__.py
@@ -6,6 +6,11 @@ class Provider(InternetProvider):
         "gmail.com",
         "hotmail.com",
         "yahoo.com",
+        # Hungarian free email providers: https://madweb.hu/a-7-legjobb-ingyenesen-hasznalhato-e-mail-szolgaltatas/ under section Magyar e-mail szolgáltatók: aka Hungarian email providers:
+        "freemail.hu",
+        "citromail.hu",
+        "indamail.hu",
+        "mailbox.hu",
     )
 
     tlds = (
