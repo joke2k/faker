e7d5cf2ee9c90058b56d393e40a91dc58506e4f7
Flavio Curella <flavio.curella@gmail.com>
2026-04-17 15:07:48 -0500

    Fix test

--- tests/providers/test_geo.py
+++ tests/providers/test_geo.py
@@ -7,10 +7,55 @@ from faker import Faker
 from faker.providers.geo.pt_PT import Provider as PtPtProvider
 
 
-class TestArDz(TestEnUS):
+class TestArDz(unittest.TestCase):
     def setUp(self):
         self.fake = Faker("ar_DZ")
         Faker.seed(0)
+
+    def test_latitude(self):
+        lat = self.fake.latitude()
+        assert isinstance(lat, Decimal)
+
+    def test_longitude(self):
+        long = self.fake.longitude()
+        assert isinstance(long, Decimal)
+
+    def test_latlng(self):
+        loc = self.fake.latlng()
+        assert isinstance(loc, tuple)
+        assert len(loc) == 2
+        assert isinstance(loc[0], Decimal)
+        assert isinstance(loc[1], Decimal)
+
+    def test_coordinate(self):
+        loc = self.fake.coordinate()
+        assert isinstance(loc, Decimal)
+
+    def test_coordinate_centered(self):
+        loc = self.fake.coordinate(center=23)
+        assert round(loc) == 23
+
+    def test_coordinate_rounded(self):
+        loc = self.fake.coordinate(center=23, radius=3)
+        assert 20 <= round(loc) <= 26
+
+    def test_location_on_land(self):
+        loc = self.fake.location_on_land()
+        assert isinstance(loc, tuple)
+        assert len(loc) == 5
+        assert Decimal(loc[0])  # Should be able to cast first two elements of tuple to Decimal
+        assert Decimal(loc[1])
+        assert isinstance(loc[2], str)  # Place is a string
+        assert isinstance(loc[3], str)  # Country code is a string
+        assert len(loc[3]) == 2  # Country code is two letters
+        assert isinstance(loc[4], str)  # Timezone is a string
+
+    def test_location_on_land_coords_only(self):
+        loc = self.fake.location_on_land(coords_only=True)
+        assert isinstance(loc, tuple)
+        assert len(loc) == 2
+        assert Decimal(loc[0])  # Should be able to cast first two elements of tuple to Decimal
+        assert Decimal(loc[1])
 
 
 class TestCsCz(unittest.TestCase):
